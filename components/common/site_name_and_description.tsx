1e886aa5547d5962e0660418e685e31bce083d70
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-04 10:24:54 +0800

    Fix Error on UI

--- components/common/site_name_and_description.tsx
+++ components/common/site_name_and_description.tsx
@@ -33,7 +33,7 @@ export default class SiteNameAndDescription extends React.PureComponent<Props> {
 
         return (
             <React.Fragment>
-                <h3 id='site_name'>{siteName}</h1>
+                <h3 id='site_name'>{siteName}</h3>
                 <h5
                     id='site_description'
                     className='color--light'
