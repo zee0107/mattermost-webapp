95cfbdfb9d7e3ff06a76082dd322a8eb194469ed
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-09 16:57:45 +0800

    Update header_footer_template.tsx

--- components/header_footer_template/header_footer_template.tsx
+++ components/header_footer_template/header_footer_template.tsx
@@ -4,7 +4,7 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import {FormattedMessage} from 'react-intl';
-import Menu from './components/menu/Navbar';
+import Menu from 'components/menu/Navbar';
 
 import {ClientConfig} from 'mattermost-redux/types/config';
 import logoLight from 'images/logoLight.png';
