312aeb3d24e4658b3daf915e859da95d94d33379
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-10 09:26:32 +0800

    Updated Nav

--- components/menu/RightNav.tsx
+++ components/menu/RightNav.tsx
@@ -17,51 +17,9 @@ type Props = {
 function RightNav(props: Props) {
   return (
     <S.Ul2 open={props.open}>
-        <NavLink
-          to='/signup_index'
-          activeStyle={{
-            fontWeight: 'bold',
-            color: '#0DADEA',
-          }}
-        >
-          <S.Li2>
-              <a href="/signup_index" className="btn buttonBgGreen">Register</a>
-          </S.Li2>
-        </NavLink>
-
-        <NavLink
-          to='/login'
-          activeStyle={{
-            fontWeight: 'bold',
-            color: '#0DADEA',
-          }}
-        >
-          <S.Li2><a href="/login" className="btn buttonBgGreen">Login</a></S.Li2>
-        </NavLink>
+      <S.Li2><a href="/signup_index" className="btn buttonBgGreen">Register</a></S.Li2>
+      <S.Li2><a href="/login" className="btn buttonBgGreen">Login</a></S.Li2>
     </S.Ul2>
-
-    /*<Router>
-      
-
-      <Switch>
-        <Route exact path='/signup_index'>
-        </Route>
-        <Route exact path='/login'>
-        </Route>
-        <Route exact path='#'>
-          <S.Icon>
-            <S.Image src={IconMario} alt='Mario' />
-          </S.Icon>
-        </Route>
-        <Route exact path='#'>
-          <S.Icon>
-            <S.Image src={IconTurtle} alt='Turtle' />
-          </S.Icon>
-        </Route>
-
-        <Redirect to='/home' />
-      </Switch>
-    </Router>*/
   );
 }
 
