65b2e11d2b0932f38cc8139641313c30ce0dd9e4
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-10 16:49:51 +0800

    Update data.js

--- components/carousel/data.js
+++ components/carousel/data.js
@@ -1,53 +1,43 @@
 
-import profile1 from 'images/profiles/profile1.png';
-import profile2 from 'images/profiles/profile2.png';
-import profile3 from 'images/profiles/profile3.png';
-import profile4 from 'images/profiles/profile4.png';
-import profile5 from 'images/profiles/profile5.png';
-import profile6 from 'images/profiles/profile6.png';
-import profile7 from 'images/profiles/profile7.png';
-import profile8 from 'images/profiles/profile8.png';
-import profile9 from 'images/profiles/profile9.png';
-
 export const data = [
   {
     name: "John Simons",
-    img: profile1,
+    img: "images/profiles/profile1.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Neo Vlance",
-    img: profile2,
+    img: "images/profiles/profile2.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Morphues Kit",
-    img: profile3,
+    img: "images/profiles/profile3.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Trinity Gulo",
-    img: profile4,
+    img: "images/profiles/profile4.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Alex Gonsala",
-    img: profile5,
+    img: "images/profiles/profile5.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Mike Tan",
-    img: profile6,
+    img: "images/profiles/profile6.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Michael Ben",
-    img: profile7,
+    img: "images/profiles/profile7.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
   {
     name: "Mika Salamat",
-    img: profile8,
+    img: "images/profiles/profile8.png",
     feed: "Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location.",
   },
 ];
