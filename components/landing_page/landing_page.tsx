8ef57d63cfb5b465197c87fcd718cce943e3db14
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-10 11:31:34 +0800

    Update Landing page

--- components/landing_page/landing_page.tsx
+++ components/landing_page/landing_page.tsx
@@ -8,6 +8,10 @@ import {ServerError} from 'mattermost-redux/types/errors';
 import {isEmail} from 'mattermost-redux/utils/helpers';
 import logoImage from 'images/logoWhite.png';
 import homeImage from 'images/homeFeed.png';
+import cImage from 'images/icons/cicon.png';
+import safeImage from 'images/icons/safeicon.png';
+import btcImage from 'images/icons/bitcoinicon.png';
+import playImage from 'images/icons/playicon.png';
 
 import BackButton from 'components/common/back_button';
 import LocalizedInput from 'components/localized_input/localized_input';
@@ -122,26 +126,38 @@ export default class LandingPage extends React.PureComponent<Props, State> {
                                 <h1 className="headerText">BRINGING THE DEFI COMMUNITY TOGETHER</h1>
                                 <h4>Nobody likes scams and Rug Pulls. Here at Crypter, we ensure that featured projects are all completely legitimate, as their ads must undergo a vetting process; this way, we can eliminate promotions of scam projects, so nobody has to suffer the consequences. Our goal is to establish a community where everyone looks out for each other, while users who are sharing scam projects will be banned permanently.</h4>
                         </div>
-                        <div className="col-lg-12">
+                        <div className="col-lg-12 marginTopBottom">
                             <div className="row">
                                 <div className="col-lg-3">
                                     <div className="col-lg-12 knowmore-box">
-                                        Test
+                                        <img src={cImage}></img>
+                                        <h5>Portfolio Tracker</h5>
+                                        <p>Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location. </p>
+                                        <a href="#">Know More</a>
                                     </div>
                                 </div>
                                 <div className="col-lg-3">
                                     <div className="col-lg-12 knowmore-box">
-                                        Test
+                                        <img src={safeImage}></img>
+                                        <h5>Social Network</h5>
+                                        <p>A Facebook-like platform for crypto enthusiasts. Share your gains and losses, flex your NFTs, or create private groups and chat with your crypto friends: Crypter is the place to do it all. Some dapps will also be integrated on this platform.</p>
+                                        <a href="#">Know More</a>
                                     </div>
                                 </div>
                                 <div className="col-lg-3">
                                     <div className="col-lg-12 knowmore-box">
-                                        Test
+                                        <img src={btcImage}></img>
+                                        <h5>Crypter Wallet</h5>
+                                        <p>The development of our own wallet will start early next year. This will have features unique to Crypter: an NFT tracker, a P2P service, and many more. A physical debit card connected to this wallet is also an option that is currently being evaluated.</p>
+                                        <a href="#">Know More</a>
                                     </div>
                                 </div>
                                 <div className="col-lg-3">
                                     <div className="col-lg-12 knowmore-box">
-                                        Test
+                                        <img src={playImage}></img>
+                                        <h5>Crypter Marketplace</h5>
+                                        <p>This will essentially be an online marketplace, where CRYPT or any other token can be used as payment. The development of Crypter Pay will be a major part of this project: Crypter Pay will become a payment processor that can be used on Wordpress and Shopify sites.</p>
+                                        <a href="#">Know More</a>
                                     </div>
                                 </div>
                             </div>
