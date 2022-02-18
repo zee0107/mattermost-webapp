494526696116be5434ec8c803c3198d8c5edc40a
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-18 15:19:47 +0800

    Update root.jsx

--- components/root/root.jsx
+++ components/root/root.jsx
@@ -25,7 +25,7 @@ import CompassThemeProvider from 'components/compass_theme_provider/compass_them
 import GlobalHeader from 'components/global_header/global_header';
 import ModalController from 'components/modal_controller';
 import {HFTRoute} from 'components/header_footer_template_route';
-import {LoggedInHFTRoute} from 'components/header_footer_login_route';
+import {LoggedInHFTRoute} from 'components/header_footer_login';
 import IntlProvider from 'components/intl_provider';
 import NeedsTeam from 'components/needs_team';
 
@@ -416,7 +416,7 @@ export default class Root extends React.PureComponent {
                         path={'/home'}
                         component={LandingPage}
                     />
-                    <LoggedInHFTRoute
+                    <LoggedInRoute
                         path={'/profile'}
                         component={ProfilePage}
                     />
