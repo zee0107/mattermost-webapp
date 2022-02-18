6c2fd17477babadf7a4d319410dce6fcd9c82b49
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-18 14:49:21 +0800

    Update header_footer_template.tsx

--- components/header_footer_template/header_footer_template.tsx
+++ components/header_footer_template/header_footer_template.tsx
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import {FormattedMessage} from 'react-intl';
 //import Menu from 'components/menu/Navbar';
-import Menu from 'components/nav_login/navlogin';
+//import Menu from 'components/nav_login/navlogin';
+import {Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 import {ClientConfig} from 'mattermost-redux/types/config';
 import logoLight from 'images/logoLight.png';
@@ -165,7 +166,34 @@ export default class NotLoggedIn extends React.PureComponent<Props> {
 
         return (
             <div className='inner-wrap' data-theme={this.state.isDark}>
-                <Menu />
+                {/*<Menu />*/}
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            </Nav>
+                            <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                </Navbar>
                 <div className='row content'>
                     {React.cloneElement(this.props.children, {mode: this.state.isDark})}
                     {/*this.props.children*/}
