a9e1d487c68c72fd94b739bb7a40cf60cabffdbd
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-18 16:21:07 +0800

    Update CenterNav.tsx

--- components/menu_login/CenterNav.tsx
+++ components/menu_login/CenterNav.tsx
@@ -1,6 +1,7 @@
 import * as S from './styles';
 
 import Logo from 'images/logoLight.png';
+import profPic from 'images/profiles/user-profile-1.png'
 
 import {
   BrowserRouter as Router,
@@ -21,10 +22,18 @@ function CenterNav(props: Props) {
         <S.Li3><hr /></S.Li3>
         <S.Li3><hr /></S.Li3>
         <S.Li3><hr /></S.Li3>
-        <S.Li3><hr /></S.Li3>
-        <S.Li3><hr /></S.Li3>
-        <S.Li2><a href="/signup_index" className="btn buttonBgGreen">Register</a></S.Li2>
-        <S.Li2><a href="/login" className="btn buttonBgWhite">Login</a></S.Li2>
+        <S.Li2><a href="/messages" className="btn buttonBgWhite"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" class="bi bi-person-plus" viewBox="0 0 16 16">
+        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+      </svg></a></S.Li2>
+        <S.Li2><a href="/messages" className="btn buttonBgWhite"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" class="bi bi-chat-right-text" viewBox="0 0 16 16">
+          <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
+          <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+        </svg></a></S.Li2>
+        <S.Li2><a href="/notification" className="btn buttonBgWhite"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" class="bi bi-bell" viewBox="0 0 16 16">
+          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+        </svg></a></S.Li2>
+        <S.Li2><a href="/profile" className="btn buttonBgWhite"><img src={profPic}></img>Evan Yates</a></S.Li2>
     </S.Ul>
 
     /*<Router>
