872dabd55723cc577f54f7efe4c3be6c501096cd
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-21 15:53:56 +0800

    Updated UI

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -149,7 +149,7 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
 
         return (
             <div>
-                <div className='col-sm-12 bodyBgElipseLanding bgGrey removePadding'>
+                <div className='col-sm-12 bodyBgElipseProfile bgGrey removePadding'>
                     <div className="inner-wrap-section">
                         <div className='d-flex'>
                             <div className="col-lg-12">
@@ -228,7 +228,36 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                 </div>
                                 <div className='col-lg-4'>
                                     <div className='col-lg-12'>
-                                        
+                                        <div className='row'>
+                                            <div className='col-lg-6'>
+                                                <div className='d-flex'>
+                                                    <div className='col-sm-2'>
+                                                        <img src={profPic} className='chat-img'></img>
+                                                    </div>
+                                                    <div className='col-sm-7'>
+                                                        <input className='form-control' />
+                                                    </div>
+                                                    <div className='col-sm-3'>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='col-lg-2'>
+                                                <select className='form-select'>
+                                                    <option value='Everyone'>Everyone</option>
+                                                    <option value='Friends'>Friends Only</option>
+                                                    <option value='Private'>Private</option>
+                                                </select>
+                                            </div>
+                                            <div className='col-lg-2'>
+                                                <a href='#' className='btn buttonBgGreen'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" class="bi bi-camera-video" viewBox="0 0 16 16">
+                                                    <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z"/>
+                                                </svg>Live</a>
+                                            </div>
+                                            <div className='col-lg-2'>
+                                                <a href='#' className='btn buttonBgGreen'>Share</a>
+                                            </div>
+                                        </div>
                                     </div>
                                     <br />
                                     <div className='col-lg-12 chat-box removePadding'>
@@ -322,153 +351,6 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                             </div>
                         </div>
                     </div>
-                    {/*<div className="inner-wrap-knowmore">
-                        <div className="col-sm-8 text-center">
-                                <h1 className="headerText">BRINGING THE DEFI COMMUNITY TOGETHER</h1>
-                                <h4 className='getSecondaryText'>Nobody likes scams and Rug Pulls. Here at Crypter, we ensure that featured projects are all completely legitimate, as their ads must undergo a vetting process; this way, we can eliminate promotions of scam projects, so nobody has to suffer the consequences. Our goal is to establish a community where everyone looks out for each other, while users who are sharing scam projects will be banned permanently.</h4>
-                        </div>
-                        <div className="col-lg-12 marginTopBottom">
-                            <div className="row">
-                                <div className="col-lg-3">
-                                    <div className="col-lg-12 knowmore-box">
-                                        <img src={cImage} className="imgIcons-knowmore"></img>
-                                        <h4 className="textBold">Portfolio Tracker</h4>
-                                        <p>Crypter’s Portfolio Tracker provides a simple overview of the current value of your DeFi cryptocurrency tokens in one location. </p>
-                                        <a href="#" className="link-knowmore">Know More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 -5 16 16">
-                                            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-                                        </svg></a>
-                                        <br></br>
-                                    </div>
-                                </div>
-                                <div className="col-lg-3">
-                                    <div className="col-lg-12 knowmore-box">
-                                        <img src={safeImage} className="imgIcons-knowmore"></img>
-                                        <h4 className="textBold">Social Network</h4>
-                                        <p>A Facebook-like platform for crypto enthusiasts. Share your gains and losses, flex your NFTs, or create private groups chat with friends.</p>
-                                        <a href="#" className="link-knowmore">Know More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 -5 16 16">
-                                            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-                                        </svg></a>
-                                        <br></br>
-                                    </div>
-                                </div>
-                                <div className="col-lg-3">
-                                    <div className="col-lg-12 knowmore-box">
-                                        <img src={btcImage} className="imgIcons-knowmore"></img>
-                                        <h4 className="textBold">Crypter Wallet</h4>
-                                        <p>The development of our own wallet will start early next year. This will have features unique to Crypter: P2P service, and more.</p>
-                                        <a href="#" className="link-knowmore">Know More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 -5 16 16">
-                                            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-                                        </svg></a>
-                                        <br></br>
-                                    </div>
-                                </div>
-                                <div className="col-lg-3">
-                                    <div className="col-lg-12 knowmore-box">
-                                        <img src={playImage} className="imgIcons-knowmore"></img>
-                                        <h4 className="textBold">Crypter Marketplace</h4>
-                                        <p>The development of Crypter Pay will be a major part of this project: It will become a payment that can be used on Wordpress and Shopify.</p>
-                                        <a href="#" className="link-knowmore">Know More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 -5 16 16">
-                                            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-                                        </svg></a>
-                                        <br></br>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <br></br>
-                    </div>
-                    <div className="inner-wrap-knowmore">
-                        <div className="col-lg-12">
-                            <div className='col-lg-12 learnmoreBox'>
-                                <div className='row'>
-                                    <div className="col-lg-9">
-                                        <h3>New In Cryptocurrency?</h3>
-                                        <h4 className='getSecondaryText'>We'll tell you what cryptocurrencies are, how they work and why you should own one right now. So let's do it.</h4>
-                                    </div>
-                                    <div className="col-lg-3 displayCenter">
-                                        <a href="/signup_index" className="btn buttonBgGreen btnLearnText">Learn &amp; Explore Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="inner-wrap-knowmore">
-                        <div className="col-lg-12 marginTopBottom">
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <h1 className="headerTextGettingStarted">How To Get Started</h1>
-                                    <h4 className='getSecondaryText'>Simple and easy way to start your investment in cryptocurrency</h4>
-                                    <br />
-                                    <a href="/signup_index" className="btn buttonBgGreen btnGettingStartedText">Getting Started</a>
-                                </div>
-                                <div className="col-lg-6">
-                                    <div className="col-lg-12 removePadding">
-                                        <div className="d-flex gettingstartedBox">
-                                            <div className="col-lg-2 text-center displayCenter">
-                                                <img src={createImage} className="gettingstartedImg"></img>
-                                            </div>
-                                            <div className="col-lg-10">
-                                                <h3 className='getStartPrimaryText'>Create Your Account</h3>
-                                                <h5 className='getSecondaryText'>Your account and personal identity are guaranteed safe.</h5>
-                                            </div>
-                                        </div>
-                                        <div className="d-flex gettingstartedBox">
-                                            <div className="col-lg-2 text-center displayCenter">
-                                                <img src={bankImage} className="gettingstartedImg"></img>
-                                            </div>
-                                            <div className="col-lg-10">
-                                                <h3 className='getStartPrimaryText'>Connect Bank Account</h3>
-                                                <h5 className='getSecondaryText'>Connect the bank account to start transactions.</h5>
-                                            </div>
-                                        </div>
-                                        <div className="d-flex gettingstartedBox">
-                                            <div className="col-lg-2 text-center displayCenter">
-                                                <img src={buildImage} className="gettingstartedImg"></img>
-                                            </div>
-                                            <div className="col-lg-10">
-                                                <h3 className='getStartPrimaryText'>Start Build Portfolio</h3>
-                                                <h5 className='getSecondaryText'>Buy and sell popular currencies and keep track of them.</h5>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="whiteBackground">
-                        <div className="inner-wrap-feedback">
-                            <div className="col-lg-12 marginTopBottom">
-                                <div className='col-lg-12'>
-                                    <div className="row">
-                                        <div className="col-lg-6 feedback-box">
-                                            <h1 className="headerFeedbackText">Engage to Earn</h1>
-                                            <h4 className='getSecondaryText'>Crypter is the first project to launch the revolutionary Engage-to-Earn system that rewards members who engage with their friends and with the community; imagine getting paid for every like, post, comment you make on your Crypter profile.</h4>
-                                        </div>
-                                        <div className="col-lg-6 feedback-box">
-                                            <Carousel />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="inner-wrap-knowmore">
-                        <div className="col-lg-12 marginBottom">
-                            <div className='col-lg-12 joinusBox'>
-                                <div className="row">
-                                    <div className="col-lg-5">
-                                        <h1>Join the largest social Crypto community</h1>
-                                        <p>Join with more 2.5M+ Global Users</p>
-                                        <a href="#" className="linkText"><img src={profilesImage}></img> and others</a>
-                                    </div>
-                                    <div className="col-lg-4"></div>
-                                    <div className="col-lg-3">
-                                        <a href="/signup_index" className="btn btnNowBgWhite">Register Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>*/}
                 </div>
             </div>
         );
