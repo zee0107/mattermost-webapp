1209634aa6bd4f16879ddd2032fec50643ebc4e3
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-21 16:45:45 +0800

    Update profile_page.tsx

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -222,8 +222,48 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                     </div>
                     <br />
                     <div className='inner-wrap-knowmore'>
-                        <div className='row'>
-                            
+                        <div className='col-lg-12'>
+                            <div className='row'>
+                                <div className='col-lg-8'>
+                                    <div className='col-lg-12'>
+                                        <div className='row'>
+                                            <div className='col-md-7 removePaddingRight'>
+                                                <div className='d-flex'>
+                                                    <div className='col-sm-2 removePadding'>
+                                                        <img src={profPic} className='chat-img'></img>
+                                                    </div>
+                                                    <div className='col-sm-7 removePadding'>
+                                                        <input className='form-control' placeholder='Whats going on, Evan?' />
+                                                    </div>
+                                                    <div className='col-sm-3 removePadding'>
+                                                    
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='col-md-5 removePaddingRight'>
+                                                <select className='form-control'>
+                                                    <option value='Everyone'><i class="bi bi-globe"></i> Everyone</option>
+                                                    <option value='Friends'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-people" viewBox="0 0 16 16">
+                                                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                                                    </svg> Friends Only</option>
+                                                    <option value='Private'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-person" viewBox="0 0 16 16">
+                                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                                                    </svg> Private</option>
+                                                </select>
+                                                <a href='#' className='btn buttonBgWhite'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-camera-video" viewBox="0 0 16 16">                                                        <path fillRule="var(--text-primary)" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z"/>
+                                                </svg> Live</a>
+                                                <a href='#' className='btn buttonBgGreen'>Share</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='col-lg-12'>
+                                        <h1>No Post</h1>
+                                    </div>
+                                </div>
+                                <div className='col-lg-4'>
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
