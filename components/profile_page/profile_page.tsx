0ee7a5fd06d0eb4d50f94ef9af18d444c916c7ec
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-21 18:42:49 +0800

    Updated Layout

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -264,18 +264,18 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                         </div>
                                     </div>
                                     <br></br>
-                                    <div className='col-lg-12 post-div'>
+                                    <div className='col-lg-12 post-div margin-top-50'>
                                         <div className='d-flex'>
                                             <div className='col-lg-2 text-center removePaddingRight'>
                                                 <img src={postPic} className='post-img'></img>
                                             </div>
                                             <div className='col-lg-9'>
-                                                <h4 className='getStartPrimaryText'>Cody Fisher</h4>
-                                                <h5 className='getSecondaryText'>New york City, Ny</h5>
+                                                <h5 className='getStartPrimaryText'>Cody Fisher</h5>
+                                                <h6 className='getSecondaryText'>New york City, Ny</h6>
                                             </div>
                                             <div className='col-lg-1'>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" fill='var(--text-primary)'/>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down margin-top-50" viewBox="0 0 16 16">
+                                                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" fill='var(--text-primary)'/>
                                                 </svg>
                                             </div>
                                         </div>
