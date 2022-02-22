9c2d5e0f5f9221c206484b57e0548c1b1a2f6c68
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-22 10:36:34 +0800

    Updated UI

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -196,7 +196,7 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className='col-lg-12 profile-menu-box'>
+                                        <div id='profileMenu' className='col-lg-12 profile-menu-box'>
                                             <div className='d-flex'>
                                                 <div className='col-lg-2 profile-menu-icon'>
                                                     <a href='#'><img src={LayoutIcon}></img></a>
@@ -278,7 +278,7 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                                 <h6 className='getSecondaryText'>New york City, Ny</h6>
                                             </div>
                                             <div className='col-lg-1'>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down margin-top-50" viewBox="0 0 16 16">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down margin-top-30" viewBox="0 0 16 16">
                                                 <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" fill='var(--text-primary)'/>
                                                 </svg>
                                             </div>
