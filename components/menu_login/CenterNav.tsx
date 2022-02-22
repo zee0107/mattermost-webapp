7975e1d115297fc132931dbba6568576baae79b5
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-22 17:03:30 +0800

    Update CenterNav.tsx

--- components/menu_login/CenterNav.tsx
+++ components/menu_login/CenterNav.tsx
@@ -26,7 +26,14 @@ function CenterNav(props: Props) {
     <S.Ul open={props.open}>
         <S.LogoUl src={Logo} alt={'Crypter'} />
         <S.Li3><hr /></S.Li3>
-        <S.Li2><input id='search' type='text' className='form-control' placeholder='Search People, Pages, Groups &amp; #Hashtag'/></S.Li2>
+        <S.Li2>
+          <div className='d-flex'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+            <input id='search' type='text' className='form-control' placeholder='Search People, Pages, Groups &amp; #Hashtag'/>
+          </div>
+        </S.Li2>
         <S.Li3><hr /></S.Li3>
         <S.Li2><a href="/messages" className="btn buttonBgWhite"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" class="bi bi-person-plus menu-align" viewBox="0 0 16 16">
         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
