29e5eafb154261fe61d7d1f9738ec3f4401f68d8
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-02-23 18:20:23 +0800

    Update index.ts

--- components/header_footer_login/index.ts
+++ components/header_footer_login/index.ts
@@ -4,15 +4,61 @@
 import {connect} from 'react-redux';
 
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
+import {bindActionCreators, Dispatch} from 'redux';
 
+import {setStatus, unsetCustomStatus} from 'mattermost-redux/actions/users';
+import {Client4} from 'mattermost-redux/client';
+import {Preferences} from 'mattermost-redux/constants';
+
+import {get, getBool} from 'mattermost-redux/selectors/entities/preferences';
+import {getCurrentUser, getStatusForUserId} from 'mattermost-redux/selectors/entities/users';
+
+import {openModal} from 'actions/views/modals';
+import {setStatusDropdown} from 'actions/views/status_dropdown';
+
+import {getCurrentUserTimezone} from 'selectors/general';
+import {makeGetCustomStatus, isCustomStatusEnabled, showStatusDropdownPulsatingDot, isCustomStatusExpired} from 'selectors/views/custom_status';
+import {isStatusDropdownOpen} from 'selectors/views/status_dropdown';
+import {GenericAction} from 'mattermost-redux/types/actions';
 import {GlobalState} from 'types/store';
 
 import LoggedInHFT from './header_footer_login';
 
-function mapStateToProps(state: GlobalState) {
-    return {
-        config: getConfig(state),
+function makeMapStateToProps() {
+    const getCustomStatus = makeGetCustomStatus();
+
+    return function mapStateToProps(state: GlobalState) {
+        const currentUser = getCurrentUser(state);
+
+        const userId = currentUser?.id;
+        const customStatus = getCustomStatus(state, userId);
+        const isMilitaryTime = getBool(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.USE_MILITARY_TIME, false);
+        return {
+            userId,
+            profilePicture: Client4.getProfilePictureUrl(userId, currentUser?.last_picture_update),
+            autoResetPref: get(state, Preferences.CATEGORY_AUTO_RESET_MANUAL_STATUS, userId, ''),
+            status: getStatusForUserId(state, userId),
+            customStatus,
+            currentUser,
+            isCustomStatusEnabled: isCustomStatusEnabled(state),
+            isCustomStatusExpired: isCustomStatusExpired(state, customStatus),
+            isMilitaryTime,
+            isStatusDropdownOpen: isStatusDropdownOpen(state),
+            showCustomStatusPulsatingDot: showStatusDropdownPulsatingDot(state),
+            timezone: getCurrentUserTimezone(state),
+            config: getConfig(state),
+        };
     };
 }
 
-export default connect(mapStateToProps)(LoggedInHFT);
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
+    return {
+        actions: bindActionCreators({
+            openModal,
+            setStatus,
+            unsetCustomStatus,
+            setStatusDropdown,
+        }, dispatch),
+    };
+}
+export default connect(makeMapStateToProps, mapDispatchToProps)(LoggedInHFT);
