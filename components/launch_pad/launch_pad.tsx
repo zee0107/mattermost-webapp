b11bb8455975e21ab004b9914daa476fa982fe40
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-04 16:02:17 +0800

    Updated QA

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -226,13 +226,49 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                                             <div className='d-flex'>
                                                 <div className='col-lg-2'>
                                                     <div className='d-flex'>
-                                                        <img src={fillImage}></img>
-                                                        <h1 className='fill-text'>1</h1>
+                                                        <img src={fillImage} className='fill-img'></img>
+                                                        <p className='fill-text'>1</p>
                                                     </div>
                                                 </div>
                                                 <div className='col-lg-10'>
-                                                    <h3>Verify Token</h3>
-                                                    <p>Please enter your verification token from your account.</p>
+                                                    <h3 className='text-primary'>Verify Token</h3>
+                                                    <p className='text-primary'>Please enter your verification token from your account.</p>
+                                                </div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-lg-2'>
+                                                    <div className='d-flex'>
+                                                        <img src={fillImage} className="fill-img"></img>
+                                                        <p className='fill-text'>2</p>
+                                                    </div>
+                                                </div>
+                                                <div className='col-lg-10'>
+                                                    <h3 className='text-primary'>Defi Launchpad Info</h3>
+                                                    <p className='text-primary'>Fill in your defi launchpad information</p>
+                                                </div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-lg-2'>
+                                                    <div className='d-flex'>
+                                                        <img src={fillImage} className="fill-img"></img>
+                                                        <p className='fill-text'>3</p>
+                                                    </div>
+                                                </div>
+                                                <div className='col-lg-10'>
+                                                    <h3 className='text-primary'>Additional Info</h3>
+                                                    <p className='text-primary'>Othre information.</p>
+                                                </div>
+                                            </div>
+                                            <div className='d-flex'>
+                                                <div className='col-lg-2'>
+                                                    <div className='d-flex'>
+                                                        <img src={fillImage} className="fill-img"></img>
+                                                        <p className='fill-text'>4</p>
+                                                    </div>
+                                                </div>
+                                                <div className='col-lg-10'>
+                                                    <h3 className='text-primary'>Finish</h3>
+                                                    <p className='text-primary'>Save all information to your launchpad.</p>
                                                 </div>
                                             </div>
                                         </div>
