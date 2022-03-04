fed87ba951c0827f4bfed804b619fbaef4026028
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-04 09:31:36 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -100,11 +100,10 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
         }
         const config = {
             method: "GET",
+            mode: "cors",
             data: sendData,
             headers: {
                 Accepts: "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET",
                 "X-CMC_PRO_API_KEY": apiKey
             }
         }
