f4cfb308f25c03b0daa62154d99e4bcd3ed98506
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-04 11:52:36 +0800

    Updated

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -163,10 +163,17 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                                         <div className='col-sm-12'>
                                             <div className='sidemenuBox'>
                                                 <ul>
-                                                    <li><a href='#'>New Feed</a></li>
+                                                    <li><a href='#'><svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M3.03033 12.4859H10.4389V14.3381H3.03033V12.4859ZM3.03033 16.1902H10.4389V18.0423H3.03033V16.1902ZM3.03033 8.78166H10.4389V10.6338H3.03033V8.78166ZM3.03033 5.07739H10.4389V6.92953H3.03033V5.07739ZM17.8474 6.92953V16.1902H14.1431V6.92953H17.8474ZM19.6995 5.07739H12.291V18.0423H19.6995V5.07739Z" fill="#FF8D08"/>
+                                                        </svg> New Feed</a>
+                                                    </li>
                                                     <li><a href='#'>Albums</a></li>
                                                     <li><a href='#'>My Groups</a></li>
-                                                    <li><a href='#'>My Pages</a><a href="#">View all</a></li>
+                                                    <li><a href='#'>
+                                                        <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M3.1911 12.1623H13.891C14.0377 12.1622 14.1813 12.1204 14.305 12.0417C14.4287 11.963 14.5274 11.8506 14.5896 11.7178C14.6518 11.585 14.6749 11.4373 14.6561 11.2918C14.6374 11.1464 14.5776 11.0093 14.4837 10.8967L11.68 7.53194L14.4837 4.16724C14.5776 4.05456 14.6374 3.91748 14.6561 3.77205C14.6749 3.62662 14.6518 3.47885 14.5896 3.34605C14.5274 3.21325 14.4287 3.10092 14.305 3.02221C14.1813 2.9435 14.0377 2.90167 13.891 2.90161H3.1911V16.7926" stroke="#FF8A00" stroke-width="1.67969" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        </svg> My Pages</a><a href="#">View all</a>
+                                                    </li>
                                                 </ul>
                                             </div>
                                         </div>
