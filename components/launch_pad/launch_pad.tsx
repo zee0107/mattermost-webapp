ecb71cdc1097a0beefa653abfe9ccd45c0774893
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-04 18:35:18 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -418,8 +418,8 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                                                         <img src={btcImage} className="current-conversion-img"></img>
                                                         <h5 className='text-primary'>BTC <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="var(--text-primary)" className="bi bi-arrow-left-right" viewBox="0 0 16 16">
                                                         <path fillRule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"/>
-                                                        </svg> USD</h3>
-                                                        <h3>9784.79</h2>
+                                                        </svg> USD</h5>
+                                                        <h3>9784.79</h3>
                                                         <p className='text-percent'><img src={trendImage}></img> 7.2%</p>
                                                     </div>
                                                     <div className='col-lg-6'>
