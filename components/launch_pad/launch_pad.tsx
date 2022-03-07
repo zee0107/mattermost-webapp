63ca86d05c6417d64e22bef8746b0973ca94b63d
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-07 13:53:17 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -101,7 +101,7 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
         uri.search = new URLSearchParams(sendData).toString();
         const config = {
             method: "GET",
-            mode: "no-cors",
+            mode: "cors",
             headers: {
                 Accepts: "application/json",
                 "Content-Type":"application/json",
