87113f85afa96e04bd0a746d9435b29b4243a2cd
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-07 13:45:46 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -105,7 +105,7 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
             headers: {
                 Accepts: "application/json",
                 "Content-Type":"application/json",
-                'Access-Control-Allow-Origin':'*'
+                "Access-Control-Allow-Origin":"*"
             }
         }
 
