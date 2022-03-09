802af36c23aafe4615dfb0814db3e1d0c6b0969f
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-09 16:10:55 +0800

    Updated QA

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -33,6 +33,7 @@ import ChannelHeaderMobile from 'components/channel_header_mobile';
 import Sidebar from 'components/sidebar';
 import SidebarRight from 'components/sidebar_right';
 import SidebarRightMenu from 'components/sidebar_right_menu';
+import { ButtonGroup } from 'react-bootstrap';
 
 type Props = {
     status?: string;
@@ -361,49 +362,62 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                                     <div className='col-sm-6'>
                                         <div id="createToken" className='col-lg-12'>
                                             <div className='d-flex margin-top-30'>
-                                                <div className='col-lg-1'>
+                                                <div className='col-lg-2'>
                                                     <div className='d-flex'>
                                                         <img src={fillImage} className='fill-img'></img>
                                                         <p className='fill-text'>1</p>
                                                     </div>
                                                 </div>
-                                                <div className='col-lg-11 removePaddingLeft'>
+                                                <div className='col-lg-10 removePaddingLeft'>
                                                     <h3 className='text-primary'>Verify Token</h3>
                                                     <p className='text-primary'>Please enter your verification token from your account.</p>
+
+                                                    <div className='create-token-box'>
+                                                        <h4>* Token Address</h4>
+                                                        <input type='text' className='form-control custom-token-input'></input>
+                                                        <div className='row'>
+                                                            <div className='col-md-6'>
+                                                                <label>Create Pool Fee: 1 BNB</label>
+                                                            </div>
+                                                            <div className='col-md-6'>
+                                                                <button type='button' className='btn buttonBgWhite'>Create Token</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                             <div className='d-flex margin-top-30'>
-                                                <div className='col-lg-1'>
+                                                <div className='col-lg-2'>
                                                     <div className='d-flex'>
                                                         <img src={fillImage} className="fill-img"></img>
                                                         <p className='fill-text'>2</p>
                                                     </div>
                                                 </div>
-                                                <div className='col-lg-11 removePaddingLeft'>
+                                                <div className='col-lg-10 removePaddingLeft'>
                                                     <h3 className='text-primary'>Defi Launchpad Info</h3>
                                                     <p className='text-primary'>Fill in your defi launchpad information</p>
                                                 </div>
                                             </div>
                                             <div className='d-flex margin-top-30'>
-                                                <div className='col-lg-1'>
+                                                <div className='col-lg-2'>
                                                     <div className='d-flex'>
                                                         <img src={fillImage} className="fill-img"></img>
                                                         <p className='fill-text'>3</p>
                                                     </div>
                                                 </div>
-                                                <div className='col-lg-11 removePaddingLeft'>
+                                                <div className='col-lg-10 removePaddingLeft'>
                                                     <h3 className='text-primary'>Additional Info</h3>
                                                     <p className='text-primary'>Othre information.</p>
                                                 </div>
                                             </div>
                                             <div className='d-flex margin-top-30'>
-                                                <div className='col-lg-1'>
+                                                <div className='col-lg-2'>
                                                     <div className='d-flex'>
                                                         <img src={fillImage} className="fill-img"></img>
                                                         <p className='fill-text'>4</p>
                                                     </div>
                                                 </div>
-                                                <div className='col-lg-11 removePaddingLeft'>
+                                                <div className='col-lg-10 removePaddingLeft'>
                                                     <h3 className='text-primary'>Finish</h3>
                                                     <p className='text-primary'>Save all information to your launchpad.</p>
                                                 </div>
