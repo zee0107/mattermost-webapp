d4b0b212b5d8d3ad7bfb05e4b348912d2bd883aa
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-10 10:12:29 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -135,13 +135,19 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
     }
 
     sideBoxRender = (code: string) => {
+        let img;
+        if(code === "BTC"){ img = (<img src={btcImage} className="current-conversion-img"></img>);}
+        if(code === "LTC"){ img = (<img src={ltcImage} className="current-conversion-img"></img>);}
+        if(code === "ETH"){ img = (<img src={ethImage} className="current-conversion-img"></img>);}
+        if(code === "BNB"){ img = (<img src={bnbImage} className="current-conversion-img"></img>);}
+
         return(
             <div className='col-sm-12'>
                 <div className='sidemenuBox'>
                     <div className='row'>
                         <div className='col-lg-6'>
                             <div className={code.toLocaleLowerCase() +"-icon"}>
-                                <img src={btcImage} className="current-conversion-img"></img>
+                                {img}
                             </div>
                             <h5 className='text-primary'>{code} <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="var(--text-primary)" className="bi bi-arrow-left-right" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"/>
