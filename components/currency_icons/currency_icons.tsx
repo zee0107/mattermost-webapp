8fa4f7a60169407dd8f3d9d21009bd0a35775580
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-14 10:13:56 +0800

    Update currency_icons.tsx

--- components/currency_icons/currency_icons.tsx
+++ components/currency_icons/currency_icons.tsx
@@ -49,7 +49,7 @@ export default class Icon extends React.PureComponent<Props, Attrs>{
         return(
             <div>
                 {this.getIcon(this.props.code).map((item,index) => 
-                    (<img src={item[key].logo} key={code+index} alt={code + '-icon'}></img>)
+                    (<img src={item.logo} key={code+index} alt={code + '-icon'}></img>)
                 )}
             </div>
         )
