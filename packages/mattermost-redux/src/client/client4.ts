ee7357dc2ad165c9718741a9786d9f3011df4753
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-17 17:19:52 +0800

    Update client4.ts

--- packages/mattermost-redux/src/client/client4.ts
+++ packages/mattermost-redux/src/client/client4.ts
@@ -3775,27 +3775,27 @@ export default class Client4 {
 
     getCryptoData = () => {
         return this.doFetch<AllListing[]>(
-            'https://crypterfighter.polywickstudio.ph/api/crypter/getcurrencyalldata',{method: 'get', headers: {'Allow-Access-Control-Oririgin' : '*'}}
+            'https://crypterfighter.polywickstudio.ph/api/crypter/getcurrencyalldata',{method: 'get', headers: {'Allow-Access-Control-Origin' : '*'}}
         );
     }
 
     getCryptoTrend = (limit: string,sort: string) => {
         return this.doFetch<TrendListing[]>(
-            `https://crypterfighter.polywickstudio.ph/api/crypter/gettrendingdata?limit=${limit}&sort=${sort}`,{method: 'get', headers: {'Allow-Access-Control-Oririgin' : '*'}}
+            `https://crypterfighter.polywickstudio.ph/api/crypter/gettrendingdata?limit=${limit}&sort=${sort}`,{method: 'get', headers: {'Allow-Access-Control-Origin' : '*'}}
         );
     }
 
 
     getCryptoGainer = (limit: string,sort: string) => {
         return this.doFetch<GainerListing[]>(
-            `https://crypterfighter.polywickstudio.ph/api/crypter/getgainersdata?limit=${limit}&sort=${sort}`,{method: 'get', headers: {'Allow-Access-Control-Oririgin' : '*'}}
+            `https://crypterfighter.polywickstudio.ph/api/crypter/getgainersdata?limit=${limit}&sort=${sort}`,{method: 'get', headers: {'Allow-Access-Control-Origin' : '*'}}
         );
     }
 
 
     getCryptoNew = (limit: string,sort: string) => {
         return this.doFetch<NewListing[]>(
-            `https://crypterfighter.polywickstudio.ph/api/crypter/getnewdata?limit=${limit}&sort=${sort}`,{method: 'get', headers: {'Allow-Access-Control-Oririgin' : '*'}}
+            `https://crypterfighter.polywickstudio.ph/api/crypter/getnewdata?limit=${limit}&sort=${sort}`,{method: 'get', headers: {'Allow-Access-Control-Origin' : '*'}}
         );
     }
 
