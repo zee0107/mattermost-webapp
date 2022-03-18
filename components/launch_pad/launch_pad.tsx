32ab04863b7b6646806ee43e39d3e2a46af55db0
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-18 13:16:49 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -175,15 +175,15 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                 <div className='col-sm-2 removePadding'>
                     <CurrencyIcons code={filtered.symbol.toString()}></CurrencyIcons>
                 </div>
-            <div className='col-sm-5'>
-                <label className='text-primary' key={i+"-gainer-name"}>{filtered.name}</label>
-                <p className='text-secondary small-font' key={i+"-gainer-symbol"}>{filtered.symbol}</p>
+                <div className='col-sm-5'>
+                    <label className='text-primary' key={i+"-gainer-name"}>{filtered.name}</label>
+                    <p className='text-secondary small-font' key={i+"-gainer-symbol"}>{filtered.symbol}</p>
+                </div>
+                <div className='col-sm-5 removePadding text-end'>
+                    <br></br>
+                    {this.render_percent(filtered.percent_change_24h.toString())}
+                </div>
             </div>
-            <div className='col-sm-5 removePadding text-end'>
-                <br></br>
-                {this.render_percent(filtered.percent_change_24h.toString())}
-            </div>
-        </div>
         ))
         return gainer;
     }
@@ -194,15 +194,15 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                 <div className='col-sm-2 removePadding'>
                     <CurrencyIcons code={filtered.symbol.toString()}></CurrencyIcons>
                 </div>
-            <div className='col-sm-5'>
-                <label className='text-primary' key={i+"-new-name"}>{filtered.name}</label>
-                <p className='text-secondary small-font' key={i+"-new-symbol"}>{filtered.symbol}</p>
+                <div className='col-sm-5'>
+                    <label className='text-primary' key={i+"-new-name"}>{filtered.name}</label>
+                    <p className='text-secondary small-font' key={i+"-new-symbol"}>{filtered.symbol}</p>
+                </div>
+                <div className='col-sm-5 removePadding text-end'>
+                    <br></br>
+                    {this.render_percent(filtered.percent_change_24h.toString())}                
+                </div>
             </div>
-            <div className='col-sm-5 removePadding text-end'>
-                <br></br>
-                {this.render_percent(filtered.percent_change_24h.toString())}                
-            </div>
-        </div>
         ))
         return newlist;
     }
@@ -211,18 +211,18 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
         var trend = this.state.trendListing.map((filtered,i)=> {
             return (
                 <div className='d-flex'>
-                <div className='col-sm-2 removePadding'>
-                    <CurrencyIcons code={filtered.symbol.toString()}></CurrencyIcons>
+                    <div className='col-sm-2 removePadding'>
+                        <CurrencyIcons code={filtered.symbol.toString()}></CurrencyIcons>
+                    </div>
+                    <div className='col-sm-5'>
+                        <label className='text-primary' key={i+"trend-name"}>{filtered.name}</label>
+                        <p className='text-secondary small-font' key={i+"trend-symbol"}>{filtered.symbol}</p>
+                    </div>
+                    <div className='col-sm-5 removePadding text-end'>
+                        <br></br>
+                        {this.render_percent(filtered.percent_change_24h.toString())}
+                    </div>
                 </div>
-                <div className='col-sm-5'>
-                    <label className='text-primary' key={i+"trend-name"}>{filtered.name}</label>
-                    <p className='text-secondary small-font' key={i+"trend-symbol"}>{filtered.symbol}</p>
-                </div>
-                <div className='col-sm-5 removePadding text-end'>
-                    <br></br>
-                    {this.render_percent(filtered.percent_change_24h.toString())}
-                </div>
-            </div>
             )
         });
         return trend;
@@ -261,25 +261,25 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                         <div className='wrap-launchpad-section'>
                             <div className='col-lg-12 removePadding'>
                                 <div className='row'>
-                                    <div className='col-sm-3' id="side_menu_left">
+                                    <div className='col-sm-3' id='side_menu_left'>
                                         <div className='col-sm-12'>
                                             <div className='sidemenuBox'>
                                                 <ul className='ul-collapse'>
-                                                    <li className='sidemenu-padding'><a href='#' className='sidemenu-item1'><svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg" className='side-menu-align'>
+                                                    <li key='news-feed' className='sidemenu-padding'><a href='#' className='sidemenu-item1'><svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg" className='side-menu-align'>
                                                         <path d="M3.03033 12.4859H10.4389V14.3381H3.03033V12.4859ZM3.03033 16.1902H10.4389V18.0423H3.03033V16.1902ZM3.03033 8.78166H10.4389V10.6338H3.03033V8.78166ZM3.03033 5.07739H10.4389V6.92953H3.03033V5.07739ZM17.8474 6.92953V16.1902H14.1431V6.92953H17.8474ZM19.6995 5.07739H12.291V18.0423H19.6995V5.07739Z" fill="#FF8D08"/>
-                                                        </svg> New Feed</a>
+                                                        </svg> News Feed</a>
                                                     </li>
-                                                    <li className='sidemenu-padding'><a href='#' className='sidemenu-item2'>
+                                                    <li key='albums' className='sidemenu-padding'><a href='#' className='sidemenu-item2'>
                                                         <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg" className='side-menu-align'>
                                                         <path d="M15.5387 3.35472V12.6154H6.27804V3.35472H15.5387ZM15.5387 1.81128H6.27804C5.42914 1.81128 4.73459 2.50583 4.73459 3.35472V12.6154C4.73459 13.4643 5.42914 14.1588 6.27804 14.1588H15.5387C16.3876 14.1588 17.0821 13.4643 17.0821 12.6154V3.35472C17.0821 2.50583 16.3876 1.81128 15.5387 1.81128ZM8.97906 9.27383L10.2833 11.0179L12.1971 8.62559L14.767 11.8437H7.04976L8.97906 9.27383ZM1.64771 4.89817V15.7023C1.64771 16.5512 2.34225 17.2457 3.19115 17.2457H13.9953V15.7023H3.19115V4.89817H1.64771Z" fill="#3F8CFF"/>
                                                         </svg> Albums</a>
                                                     </li>
-                                                    <li className='sidemenu-padding'><a href='#' className='sidemenu-item3'>
+                                                    <li key='My-groups' className='sidemenu-padding'><a href='#' className='sidemenu-item3'>
                                                         <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg" className='side-menu-align'>
                                                         <path d="M3.95634 12.8045C4.97502 12.8045 5.80848 11.971 5.80848 10.9524C5.80848 9.93368 4.97502 9.10022 3.95634 9.10022C2.93767 9.10022 2.10421 9.93368 2.10421 10.9524C2.10421 11.971 2.93767 12.8045 3.95634 12.8045ZM5.0028 13.8232C4.66015 13.7676 4.31751 13.7306 3.95634 13.7306C3.03954 13.7306 2.16903 13.925 1.38188 14.2677C1.04637 14.4111 0.760415 14.6499 0.559546 14.9545C0.358677 15.2591 0.251757 15.616 0.252076 15.9809V17.4348H4.41938V15.9439C4.41938 15.1752 4.63237 14.4529 5.0028 13.8232V13.8232ZM18.7734 12.8045C19.7921 12.8045 20.6255 11.971 20.6255 10.9524C20.6255 9.93368 19.7921 9.10022 18.7734 9.10022C17.7547 9.10022 16.9213 9.93368 16.9213 10.9524C16.9213 11.971 17.7547 12.8045 18.7734 12.8045ZM22.4777 15.9809C22.4777 15.2308 22.0332 14.564 21.3479 14.2677C20.5358 13.9133 19.6594 13.7305 18.7734 13.7306C18.4122 13.7306 18.0696 13.7676 17.727 13.8232C18.0974 14.4529 18.3104 15.1752 18.3104 15.9439V17.4348H22.4777V15.9809ZM15.2914 13.4064C14.2079 12.9249 12.8744 12.573 11.3649 12.573C9.85539 12.573 8.52185 12.9341 7.43835 13.4064C6.95036 13.6264 6.53665 13.9832 6.24736 14.4336C5.95807 14.884 5.80561 15.4086 5.80848 15.9439V17.4348H16.9213V15.9439C16.9213 14.8511 16.2915 13.8509 15.2914 13.4064V13.4064ZM7.72543 15.5827C7.80878 15.3697 7.84582 15.2215 8.56815 14.9437C9.46644 14.5918 10.411 14.4251 11.3649 14.4251C12.3187 14.4251 13.2633 14.5918 14.1616 14.9437C14.8747 15.2215 14.9117 15.3697 15.0043 15.5827H7.72543ZM11.3649 8.17415C11.8742 8.17415 12.2909 8.59088 12.2909 9.10022C12.2909 9.60956 11.8742 10.0263 11.3649 10.0263C10.8555 10.0263 10.4388 9.60956 10.4388 9.10022C10.4388 8.59088 10.8555 8.17415 11.3649 8.17415ZM11.3649 6.32202C9.82761 6.32202 8.58668 7.56295 8.58668 9.10022C8.58668 10.6375 9.82761 11.8784 11.3649 11.8784C12.9021 11.8784 14.1431 10.6375 14.1431 9.10022C14.1431 7.56295 12.9021 6.32202 11.3649 6.32202Z" fill="#2CD889"/>
                                                         </svg> My Groups</a>
                                                     </li>
-                                                    <li className='sidemenu-padding'>
+                                                    <li key='my-pages' className='sidemenu-padding'>
                                                         <div className='d-flex'>
                                                             <a href='#' className='sidemenu-item1'>
                                                             <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg" className='side-menu-align'>
@@ -296,26 +296,26 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                                                 <input type="checkbox" id="list-item-1"></input>
                                                 <label htmlFor="list-item-1" className="first collapsible-label-title"><img src={rocketImage} className="sidemenu-title-img"></img>Launchpad</label>
                                                 <ul className='ul-collapse'>
-                                                    <li className='sidemenu-padding'><a href="#" className='list-sidemenu-a'>Create Launchpad</a></li>
-                                                    <li className='sidemenu-padding'>
+                                                    <li key='create-launchpad' className='sidemenu-padding'><a href="#" className='list-sidemenu-a'>Create Launchpad</a></li>
+                                                    <li key='projects' className='sidemenu-padding'>
                                                         <input type="checkbox" id="list-item-2"></input>
                                                         <label htmlFor="list-item-2" className="first collapsible-label">Projects</label>
                                                         <ul className='ul-collapse'>
-                                                            <li><a href="#" className='list-sidemenu-b'>Live</a></li>
-                                                            <li><a href="#" className='list-sidemenu-b'>Upcoming</a></li>
+                                                            <li key='live'><a href="#" className='list-sidemenu-b'>Live</a></li>
+                                                            <li key='upcoming'><a href="#" className='list-sidemenu-b'>Upcoming</a></li>
                                                         </ul>
                                                     </li>
-                                                    <li className='sidemenu-padding'>
+                                                    <li key='crypter-lock' className='sidemenu-padding'>
                                                         <input type="checkbox" id="list-item-3"></input>
                                                         <label htmlFor="list-item-3" className="first collapsible-label">Crypter Lock</label>
                                                         <ul className='ul-collapse'>
-                                                            <li><a href="#" className='list-sidemenu-b'>Create Lock</a></li>
-                                                            <li><a href="#" className='list-sidemenu-b'>Token</a></li>
-                                                            <li><a href="#" className='list-sidemenu-b'>Liquidity</a></li>
+                                                            <li key='create-lock'><a href="#" className='list-sidemenu-b'>Create Lock</a></li>
+                                                            <li key='token'><a href="#" className='list-sidemenu-b'>Token</a></li>
+                                                            <li key='liquidity'><a href="#" className='list-sidemenu-b'>Liquidity</a></li>
                                                         </ul>
                                                     </li>
-                                                    <li className='sidemenu-padding'><a href="#" className='list-sidemenu-a'>KYC &amp; Audit</a></li>
-                                                    <li className='sidemenu-padding'><a href="#" className='list-sidemenu-a'>Docs</a></li>
+                                                    <li key='kyc-audit' className='sidemenu-padding'><a href="#" className='list-sidemenu-a'>KYC &amp; Audit</a></li>
+                                                    <li key='docs' className='sidemenu-padding'><a href="#" className='list-sidemenu-a'>Docs</a></li>
                                                 </ul>
                                             </div>
                                         </div>
