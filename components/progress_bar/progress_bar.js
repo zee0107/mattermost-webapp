0d9466e96e2f41830cc648cb2688b663aada66eb
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-21 15:30:49 +0800

    Updated QA

--- components/progress_bar/progress_bar.js
+++ components/progress_bar/progress_bar.js
@@ -2,7 +2,15 @@ import React from 'react'
   
 const Progress_bar = ({progress}) => {
 
-      const progresstext = {
+    const Childdiv = {
+        height: '100%',
+        width: `${progress}%`,
+        backgroundColor: '#44cc4b',
+        borderRadius:40,
+        textAlign: 'right'
+      }
+
+    const progresstext = {
         padding: 10,
         color: 'black',
         fontWeight: 900
@@ -10,8 +18,8 @@ const Progress_bar = ({progress}) => {
         
     return (
         <div className='containter-bar'>
-            <div className='filler-bar'>
-            <span style={progresstext}>{`${progress}%`}</span>
+            <div style={Childdiv}>
+           {/*<span style={progresstext}>{`${progress}%`}</span>*/}
         </div>
         </div>
     )
