ca227d68ec1778a94bd7dea454b157155122d154
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-21 17:36:07 +0800

    Update projects_upcoming.tsx

--- components/projects_upcoming/projects_upcoming.tsx
+++ components/projects_upcoming/projects_upcoming.tsx
@@ -107,7 +107,7 @@ export default class ProjectsUpcoming extends React.PureComponent<Props, State> 
                                         <div className='col-md-9 text-end'>
                                             <label className='text-upcoming small sale-upcoming-box'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff8d08" className="bi bi-dot align-svg" viewBox="0 0 16 16">
                                             <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-                                            </svg>&nbsp;Sale Live</label>
+                                            </svg>&nbsp;Upcoming</label>
                                         </div>
                                     </div>
                                     <div className='col-md-12'>
