a7eae36ecca2814008f570feecab3984e615ecf4
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-22 11:51:30 +0800

    Updated UI

--- components/projects_live/projects_live.tsx
+++ components/projects_live/projects_live.tsx
@@ -344,7 +344,12 @@ export default class ProjectsLive extends React.PureComponent<Props, State> {
                     <button type='button' className='btn buttonBgWhite create-lock-btn'>Create</button>
                 </div>
                 <div id='mobileProjectFilter' className='col-md-12'>
-                    <input id='seachInput' type='text' className='form-control search-filter-input' placeholder='Enter Token Name or Token Symbol'></input>
+                    <div className='d-flex'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                        <input id='seachInput' type='text' className='form-control search-filter-input' placeholder='Enter Token Name or Token Symbol'></input>
+                    </div>
                     <br></br>
                     <div className='d-flex'>
                         <div className='col-md-6 width-50 removePaddingLeft'>
@@ -373,7 +378,12 @@ export default class ProjectsLive extends React.PureComponent<Props, State> {
                     <div className='search-filter-box'>
                         <div className='row'>
                             <div className='col-md-7'>
-                                <input id='seachInput' type='text' className='form-control search-filter-input' placeholder='Enter Token Name or Token Symbol'></input>
+                                <div className='d-flex'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                    </svg>
+                                    <input id='seachInput' type='text' className='form-control search-filter-input' placeholder='Enter Token Name or Token Symbol'></input>
+                                </div>
                             </div>
                             <div className='col-md-2'>
                                 <label className='small text-secondary'>filter By</label>
