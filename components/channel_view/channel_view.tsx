a7f4defe98b27aa705fac25c7abb7719e95488a7
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-24 17:13:21 +0800

    Update channel_view.tsx

--- components/channel_view/channel_view.tsx
+++ components/channel_view/channel_view.tsx
@@ -250,63 +250,65 @@ export default class ChannelView extends React.PureComponent<Props, State> {
                             </div>
                         </div>
                     </div>
+                    <div className='col-md-12 mtop-10'>
                     <div
-                    ref={this.channelViewRef}
-                    id='app-content'
-                    className='app__content'
-                >
-                    <div className='col-md-12 share-div'>
-                        <FileUploadOverlay overlayType='center'/>
-                        {/*<ChannelHeader
-                        {...this.props}
-                        />*/}
-                        <div className='d-flex'>
-                            <div className='col-md-6 share-div-input removePaddingRight'>
-                                <div className='d-flex'>
-                                    <div className='col-sm-2 removePadding'>
-                                        {this.renderProfilePicture('md')}
-                                        </div>
-                                        <div className='col-sm-10 removePadding'>
-                                            {createPost}
+                        ref={this.channelViewRef}
+                        id='app-content'
+                        className='app__content'
+                        >
+                        <div className='col-md-12 share-div'>
+                            <FileUploadOverlay overlayType='center'/>
+                            {/*<ChannelHeader
+                            {...this.props}
+                            />*/}
+                            <div className='d-flex'>
+                                <div className='col-md-6 share-div-input removePaddingRight'>
+                                    <div className='d-flex'>
+                                        <div className='col-sm-2 removePadding'>
+                                            {this.renderProfilePicture('md')}
+                                            </div>
+                                            <div className='col-sm-10 removePadding'>
+                                                {createPost}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className='col-lg-3 removePaddingRight margin-top-share'>
-                                    <select id='selectDesktop' className='share-select small'>
-                                        <option value='Everyone'>&#127760; Everyone</option>
-                                        <option value='Friends'>&#128101; Friends Only</option>
-                                        <option value='Private'>&#128100; Private</option>
-                                    </select>
-                                    <select id='selectMobile' className='share-select'>
-                                        <option value='Everyone'>&#127760;</option>
-                                        <option value='Friends'>&#128101;</option>
-                                        <option value='Private'>&#128100;</option>
-                                    </select>
-                                </div>
-                                <div className='col-md-3 removePaddingRight margin-top-share'>
-                                    <div id='buttonsDesktop'>
-                                        <a href='#' className='btn buttonBgWhite btn-margin-right btn-sm'><svg xmlns="http://www.w3.org/2000/svg" width="10" height="12" fill="#44cc4b" className="bi bi-camera-video" viewBox="0 -2 16 16">
-                                        <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z" fill="#44cc4b"/>
-                                        </svg> Live</a>
-                                        <a href='#' className='btn buttonBgGreen btn-padding btn-sm'>Share</a>
+                                    <div className='col-lg-3 removePaddingRight margin-top-share'>
+                                        <select id='selectDesktop' className='share-select small'>
+                                            <option value='Everyone'>&#127760; Everyone</option>
+                                            <option value='Friends'>&#128101; Friends Only</option>
+                                            <option value='Private'>&#128100; Private</option>
+                                        </select>
+                                        <select id='selectMobile' className='share-select'>
+                                            <option value='Everyone'>&#127760;</option>
+                                            <option value='Friends'>&#128101;</option>
+                                            <option value='Private'>&#128100;</option>
+                                        </select>
                                     </div>
-                                    <div id='buttonsMobile'>
-                                            <a href='#' className='btn-margin-right'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#44cc4b" className="bi bi-camera-video" viewBox="0 0 16 16">
-                                            <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z" fill="var(--text-primary)"/>
+                                    <div className='col-md-3 removePaddingRight margin-top-share'>
+                                        <div id='buttonsDesktop'>
+                                            <a href='#' className='btn buttonBgWhite btn-margin-right btn-sm'><svg xmlns="http://www.w3.org/2000/svg" width="10" height="12" fill="#44cc4b" className="bi bi-camera-video" viewBox="0 -2 16 16">
+                                            <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z" fill="#44cc4b"/>
+                                            </svg> Live</a>
+                                            <a href='#' className='btn buttonBgGreen btn-padding btn-sm'>Share</a>
+                                        </div>
+                                        <div id='buttonsMobile'>
+                                                <a href='#' className='btn-margin-right'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#44cc4b" className="bi bi-camera-video" viewBox="0 0 16 16">
+                                                <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z" fill="var(--text-primary)"/>
+                                                </svg></a>
+                                                <a href='#' className='btn-padding'><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                                <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" fill="var(--text-primary)"/>
+                                                <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" fill="var(--text-primary)"/>
                                             </svg></a>
-                                            <a href='#' className='btn-padding'><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                                            <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" fill="var(--text-primary)"/>
-                                            <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" fill="var(--text-primary)"/>
-                                        </svg></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='col-md-12 chat-box'>
-                            <DeferredPostView
-                                channelId={this.props.channelId}
-                                focusedPostId={this.state.focusedPostId}
-                            />
+                            <div className='col-md-12 chat-box'>
+                                <DeferredPostView
+                                    channelId={this.props.channelId}
+                                    focusedPostId={this.state.focusedPostId}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
