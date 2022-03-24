28718778b56c3421d6543428993243608c90979f
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-24 17:28:07 +0800

    Update channel_view.tsx

--- components/channel_view/channel_view.tsx
+++ components/channel_view/channel_view.tsx
@@ -224,7 +224,71 @@ export default class ChannelView extends React.PureComponent<Props, State> {
         return (
             <div className='row'>
                 <div className='col-md-8'>
-                    <UserStory></UserStory>
+                    <div className='col-md-12 chat-box mtop-10'>
+                        <div className='d-flex'>
+                            <div className='col-md-2 mtop-10'>
+                                <div className='position-absolute'>
+                                    <a href="#" className='onClickstory'>
+                                        {this.renderProfilePicture('xl')}
+                                    </a>
+                                </div>
+                                <div className='badges-online-plus rounded-circle onClickstory position-relative'><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="#fff" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                                </svg></div>
+                                <small className='firstname-title-story'>Your story</small>
+                            </div>
+                            <div className='col-md-2 mtop-10'>
+                                <div className='position-absolute'>
+                                    <a href="#" className='onClickstory'>
+                                        <img className="Avatar Avatar-xl" src={profPic} alt="Username" title="Username"/>
+                                    </a>
+                                </div>
+                                <div className="badges-offline-plus rounded-circle position-relative"></div>
+                                <small className="firstname-title-story mt-5">John Lloyd</small>
+                            </div>
+                            <div className='col-md-2 mtop-10'>
+                                <div className='position-absolute'>
+                                    <a href="#" className='onClickstory'>
+                                    <img className="Avatar Avatar-xl" src={postPic} alt="Username" title="Username"/>
+                                    </a>
+                                </div>
+                                <div className="badges-offline-plus rounded-circle position-relative"></div>
+                                <small className="firstname-title-story mt-5">Cody Fisher</small>
+                            </div>
+                            <div className='col-md-2 mtop-10'>
+                                <div className='position-absolute'>
+                                    <a href="#" className='onClickstory'>
+                                        <img className="Avatar Avatar-xl" src={postPic2} alt="Username" title="Username"/>
+                                    </a>
+                                </div>
+                                <div className="badges-offline-plus rounded-circle position-relative"></div>
+                                <small class="firstname-title-story mt-5">Ann Isable</small>
+                            </div>
+                            <div className='col-md-2 mtop-10'>
+                                <div className='position-absolute'>
+                                    <a href="#" className='onClickstory'>
+                                        <img className="Avatar Avatar-xl" src={postImage} alt="Username" title="Username"/>
+                                    </a>
+                                </div>
+                                <div className="badges-offline-plus rounded-circle position-relative"></div>
+                                <small className="firstname-title-story mt-5">Jade sue</small>
+                            </div>
+                            <div className='col-md-2 mtop-10'>
+                                <div className='position-absolute'>
+                                    <a href="#" className='onClickstory'>
+                                        <img className="Avatar Avatar-xl" src={postImage2} alt="Username" title="Username"/>
+                                    </a>
+                                </div>
+                                <div className="badges-offline-plus rounded-circle position-relative"></div>
+                                <small className="firstname-title-story mt-5">Mig Yu</small>
+                            </div>
+                            <div className="next-arrow-story">
+                                <a className="onAllstory"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#fff" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg></a>
+                            </div>
+                        </div>
+                    </div>
                     <div className='col-md-12 profile-menu-box-mobile width-100'>
                         <div className='d-flex'>
                             <div className='col-lg-2 profile-menu-icon'>
