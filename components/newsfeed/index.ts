187c8dd2ef747040f52aa868b31f5a60ce7193fa
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-24 13:21:06 +0800

    Updated QA

--- components/newsfeed/index.ts
+++ components/newsfeed/index.ts
@@ -24,6 +24,7 @@ import NewsFeed from './newsfeed'
 
 function makeMapStateToProps() {
     const getCustomStatus = makeGetCustomStatus();
+    const getMessageInHistoryItem = makeGetMessageInHistoryItem(Posts.MESSAGE_TYPES.POST as any);
 
     return function mapStateToProps(state: GlobalState) {
         const currentUser = getCurrentUser(state);
@@ -31,6 +32,32 @@ function makeMapStateToProps() {
         const userId = currentUser?.id;
         const customStatus = getCustomStatus(state, userId);
         const isMilitaryTime = getBool(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.USE_MILITARY_TIME, false);
+
+        const config = getConfig(state);
+        const license = getLicense(state);
+        const currentChannel = getCurrentChannel(state) || {};
+        const currentChannelTeammateUsername = getUser(state, currentChannel.teammate_id || '')?.username;
+        const draft = getPostDraft(state, StoragePrefixes.DRAFT, currentChannel.id);
+        const latestReplyablePostId = getLatestReplyablePostId(state);
+        const currentChannelMembersCount = getCurrentChannelStats(state) ? getCurrentChannelStats(state).member_count : 1;
+        const tutorialStep = getInt(state, Preferences.TUTORIAL_STEP, getCurrentUserId(state), TutorialSteps.FINISHED);
+        const enableEmojiPicker = config.EnableEmojiPicker === 'true';
+        const enableGifPicker = config.EnableGifPicker === 'true';
+        const enableConfirmNotificationsToChannel = config.EnableConfirmNotificationsToChannel === 'true';
+        const currentUserId = getCurrentUserId(state);
+        const userIsOutOfOffice = getStatusForUserId(state, currentUserId) === UserStatuses.OUT_OF_OFFICE;
+        const badConnection = connectionErrorCount(state) > 1;
+        const isTimezoneEnabled = config.ExperimentalTimezone === 'true';
+        const shortcutReactToLastPostEmittedFrom = getShortcutReactToLastPostEmittedFrom(state);
+        const canPost = haveICurrentChannelPermission(state, Permissions.CREATE_POST);
+        const useChannelMentions = haveICurrentChannelPermission(state, Permissions.USE_CHANNEL_MENTIONS);
+        const isLDAPEnabled = license?.IsLicensed === 'true' && license?.LDAPGroups === 'true';
+        const useGroupMentions = isLDAPEnabled && haveICurrentChannelPermission(state, Permissions.USE_GROUP_MENTIONS);
+        const channelMemberCountsByGroup = selectChannelMemberCountsByGroup(state, currentChannel.id);
+        const currentTeamId = getCurrentTeamId(state);
+        const groupsWithAllowReference = useGroupMentions ? getAssociatedGroupsForReferenceByMention(state, currentTeamId, currentChannel.id) : null;
+        const enableTutorial = config.EnableTutorial === 'true';
+        const showTutorialTip = enableTutorial && tutorialStep === TutorialSteps.POST_POPOVER;
         return {
             userId,
             profilePicture: Client4.getProfilePictureUrl(userId, currentUser?.last_picture_update),
@@ -47,6 +74,41 @@ function makeMapStateToProps() {
             lhsOpen: getIsLhsOpen(state),
             rhsOpen: getIsRhsOpen(state),
             rhsMenuOpen: getIsRhsMenuOpen(state),
+
+            currentTeamId,
+            currentChannel,
+            currentChannelTeammateUsername,
+            currentChannelMembersCount,
+            currentUserId,
+            codeBlockOnCtrlEnter: getBool(state, PreferencesRedux.CATEGORY_ADVANCED_SETTINGS, 'code_block_ctrl_enter', true),
+            ctrlSend: getBool(state, Preferences.CATEGORY_ADVANCED_SETTINGS, 'send_on_ctrl_enter'),
+            fullWidthTextBox: get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.CHANNEL_DISPLAY_MODE, Preferences.CHANNEL_DISPLAY_MODE_DEFAULT) === Preferences.CHANNEL_DISPLAY_MODE_FULL_SCREEN,
+            showTutorialTip,
+            messageInHistoryItem: getMessageInHistoryItem(state),
+            draft,
+            latestReplyablePostId,
+            locale: getCurrentLocale(state),
+            currentUsersLatestPost: getCurrentUsersLatestPost(state, ''),
+            canUploadFiles: canUploadFiles(config),
+            enableEmojiPicker,
+            enableGifPicker,
+            enableConfirmNotificationsToChannel,
+            maxPostSize: parseInt(config.MaxPostSize || '', 10) || Constants.DEFAULT_CHARACTER_LIMIT,
+            userIsOutOfOffice,
+            rhsExpanded: getIsRhsExpanded(state),
+            emojiMap: getEmojiMap(state),
+            badConnection,
+            isTimezoneEnabled,
+            shortcutReactToLastPostEmittedFrom,
+            canPost,
+            useChannelMentions,
+            shouldShowPreview: showPreviewOnCreatePost(state),
+            groupsWithAllowReference,
+            useGroupMentions,
+            channelMemberCountsByGroup,
+            isLDAPEnabled,
+            tutorialStep,
+            markdownPreviewFeatureIsEnabled: isFeatureEnabled(Constants.PRE_RELEASE_FEATURES.MARKDOWN_PREVIEW, state),
         };
     };
 }
