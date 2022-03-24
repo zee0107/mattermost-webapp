c3eef8a7e65f0a32eaa6d81be29f3f2cf6a157cc
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-24 15:40:05 +0800

    Update launch_pad.tsx

--- components/launch_pad/launch_pad.tsx
+++ components/launch_pad/launch_pad.tsx
@@ -140,10 +140,10 @@ export default class LaunchPad extends React.PureComponent<Props, State> {
                     <div>
                         <div className='col-lg-12 removePadding'>
                             <div className='row'>
-                                <div className='col-sm-10'>
+                                <div className='col-sm-8'>
                                     {middleRender}
                                 </div>
-                                <div className='col-sm-2' id="side_menu_right">
+                                <div className='col-sm-4' id="side_menu_right">
                                     {btcContent}
                                     {ltcContent}
                                     {ethContent}
