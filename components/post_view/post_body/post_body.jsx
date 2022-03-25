2f6d053ba9100dc4f86fc0bb266646332a330774
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-25 14:35:14 +0800

    Update post_body.jsx

--- components/post_view/post_body/post_body.jsx
+++ components/post_view/post_body/post_body.jsx
@@ -192,7 +192,7 @@ export default class PostBody extends React.PureComponent<Props,State> {
         }
 
         const {isMobile, isReadOnly, collapsedThreadsEnabled} = this.props;
-        const hover = true;/*this.props.hover || this.state.showEmojiPicker || this.state.showDotMenu || this.state.showOptionsMenuWithoutHover*/;
+        const hover = false;/*this.props.hover || this.state.showEmojiPicker || this.state.showDotMenu || this.state.showOptionsMenuWithoutHover*/;
 
         const showCommentIcon = fromAutoResponder ||
         (!isSystemMessage && (isMobile || hover || (!post.root_id && Boolean(this.props.hasReplies)) || this.props.isFirstReply));
