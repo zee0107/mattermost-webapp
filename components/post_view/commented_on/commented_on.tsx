086437c0ec35ab3cd9d57f9eb6f5f1ee4aaccc3b
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-28 09:45:58 +0800

    Update commented_on.tsx

--- components/post_view/commented_on/commented_on.tsx
+++ components/post_view/commented_on/commented_on.tsx
@@ -73,7 +73,7 @@ export default class CommentedOn extends PureComponent<Props> {
             >
                 <span>
                     <FormattedMessage
-                        id='post_body.commentedOn'
+                        id='commentedOn'
                         defaultMessage="Commented on {name}'s post: "
                         values={{
                             name: <a className='theme user_name'>{parentUserProfile}</a>,
