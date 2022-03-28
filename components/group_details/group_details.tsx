ed494bb1870ba8dbc65b0613a300d5f30d44dad1
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-28 17:22:04 +0800

    Update group_details.tsx

--- components/group_details/group_details.tsx
+++ components/group_details/group_details.tsx
@@ -44,10 +44,8 @@ export default class GroupsDetails extends React.PureComponent<Props, State> {
 
     render= (): JSX.Element => {
         const {channelId} = this.props;
-        console.log(this.state.data);
         return (
             <label className='text-count-members'>
-                {this.state.data.member_count}
                 {/*this.state.data.map((item, index) => {
                    return item.member_count 
             })*/}
