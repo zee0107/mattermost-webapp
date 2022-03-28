6a7b01ff6f789690bda9ebdf48b10cd123cf6c8c
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-28 10:34:00 +0800

    Updated

--- components/groups/groups.tsx
+++ components/groups/groups.tsx
@@ -100,7 +100,7 @@ export default class MyGroups extends React.PureComponent<Props, State> {
                                 <a className='btn btn-md float-start onMyjoined'><label className='ms-2'>Joined</label></a>
                             </div>
                             </div>
-                            <a className='float-end rounded onCreategroups negative-margin-top'>
+                            <a className='float-end onCreategroups negative-margin-top'>
                                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#fff' className='bi bi-plus side-menu-align' viewBox='0 0 16 16'>
                                 <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/>
                             </svg> Create</a>
