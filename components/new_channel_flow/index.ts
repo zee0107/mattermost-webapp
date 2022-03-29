7642e108454d8095216cbfd2300a6db7f077d790
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-29 10:51:01 +0800

    Update index.ts

--- components/new_channel_flow/index.ts
+++ components/new_channel_flow/index.ts
@@ -20,7 +20,7 @@ import NewChannelFlow, {Props} from './new_channel_flow';
 
 function mapStateToProps(state: GlobalState) {
     const currentTeam = getCurrentTeam(state);
-    if (currentTeam === null){
+    if (currentTeam === undefined){
         const team = Client4.getTeam('5meubtskybn1bg7iyfx7x4cm9c');
         if(team != null){
             Promise.resolve(team).then(value => { currentTeam = value; })
