3c81d6083869de88283ac43c40be875b58754b34
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-03-30 14:00:16 +0800

    Update channels.ts

--- packages/mattermost-redux/src/actions/channels.ts
+++ packages/mattermost-redux/src/actions/channels.ts
@@ -688,8 +688,8 @@ export function leaveChannelNew(channelId: string): ActionFunc {
             const state = getState();
             const {currentUserId} = state.entities.users;
             const {channels, myMembers} = state.entities.channels;
-            const channel = Client4.getChannel(channelId);
-            const member = Client4.getChannelMember(channel.id, currentUserId);
+            const channel = await Client4.getChannel(channelId);
+            const member = await Client4.getChannelMember(channel.id, currentUserId);
     
             Client4.trackEvent('action', 'action_channels_leave', {channel_id: channelId});
     
