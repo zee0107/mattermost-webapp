c2967d9c2772ffa28ceba0ff9661b5c780e3aefd
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-01 11:56:45 +0800

    Update group_header.tsx

--- components/group_header/group_header.tsx
+++ components/group_header/group_header.tsx
@@ -41,6 +41,8 @@ export default class GroupsHeader extends React.PureComponent<Props, State> {
     componentDidMount = () =>{
         const ThemeValue = window.localStorage.getItem('theme');
         this.setState({isDark: ThemeValue});
+
+        this._loadAsyncData(this.props.channelId);
     }
 
     componentDidUpdate(prevProps){
