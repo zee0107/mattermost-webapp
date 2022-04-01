c5680ee2e03f0e702822fc6c3cd83d3416b83680
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-01 15:07:05 +0800

    Update group_header.tsx

--- components/group_header/group_header.tsx
+++ components/group_header/group_header.tsx
@@ -78,7 +78,7 @@ export default class GroupsHeader extends React.PureComponent<Props, State> {
 
     getImage = async (channel: string) => {
         const res = await fetch(`https://localhost:44312/api/crypter/coverimg?id=${channel}`);
-        console.log(await res.json());
+        console.log(await res.body());
         const imageBlob = await res.blob();
         const imageObjectURL = URL.createObjectURL(imageBlob);
         this.setState({img_url: imageObjectURL});
