f2cbd2758dafd484e3ea52024aaedcd9625b08bd
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-06 11:23:46 +0800

    Update profile_page.tsx

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -353,8 +353,8 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                                     >
                                                         What's going on, {currentUser.first_name} {currentUser.last_name}
                                                     </ToggleModalButtonRedux>*/}
-                                                <input type='text' className='form-control input-search-crypter-whats-going-on onCreatepost mt-1' placeholder={`What's going on, ${currentUser.first_name} ${currentUser.last_name}`} aria-label={`What's going on, ${currentUser.first_name} ${currentUser.last_name}`} aria-describedby='basic-addon55' data-bs-toggle='modal' data-bs-target='#staticBackdrop' />
-                                                <span className='input-group-text input-search-crypter-whats-going-on onPhotoaddpost mt-1' id='basic-addon33' data-bs-toggle='modal' data-bs-target='#staticBackdrop'>
+                                                <input type='text' className='form-control input-search-crypter-whats-going-on onCreatepost mt-1' placeholder={`What's going on, ${currentUser.first_name} ${currentUser.last_name}`} aria-label={`What's going on, ${currentUser.first_name} ${currentUser.last_name}`} aria-describedby='basic-addon55' data-toggle='modal' data-target='#staticBackdrop' />
+                                                <span className='input-group-text input-search-crypter-whats-going-on onPhotoaddpost mt-1' id='basic-addon33' data-toggle='modal' data-target='#staticBackdrop'>
                                                     <a href='#'><svg width='21' height='21' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
                                                         <path d='M13.7555 4.41165H11.7685L10.6215 3.15802H6.86061V4.41165H10.0699L11.217 5.66527H13.7555V13.187H3.72656V7.54571H2.47293V13.187C2.47293 13.8765 3.03706 14.4406 3.72656 14.4406H13.7555C14.445 14.4406 15.0092 13.8765 15.0092 13.187V5.66527C15.0092 4.97578 14.445 4.41165 13.7555 4.41165ZM5.60699 9.42614C5.60699 11.1561 7.01105 12.5602 8.74105 12.5602C10.4711 12.5602 11.8751 11.1561 11.8751 9.42614C11.8751 7.69614 10.4711 6.29208 8.74105 6.29208C7.01105 6.29208 5.60699 7.69614 5.60699 9.42614ZM8.74105 7.54571C9.77529 7.54571 10.6215 8.3919 10.6215 9.42614C10.6215 10.4604 9.77529 11.3066 8.74105 11.3066C7.70681 11.3066 6.86061 10.4604 6.86061 9.42614C6.86061 8.3919 7.70681 7.54571 8.74105 7.54571ZM3.72656 4.41165H5.60699V3.15802H3.72656V1.27759H2.47293V3.15802H0.592499V4.41165H2.47293V6.29208H3.72656V4.41165Z' fill='var(--text-primary)'/>
                                                     </svg></a></span>
@@ -513,13 +513,13 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                     </div>
                 </div>
 
-                <div className='modal postcontent' id='staticBackdrop' data-bs-backdrop='static' data-bs-keyboard='false' tabIndex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
+                <div className='modal postcontent' id='staticBackdrop' data-backdrop='static' data-keyboard='false' tabIndex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
                     <div className='modal-dialog modal-dialog-centered'>
                         <div className='modal-content'>
                             <form className='form'>
                                 <div className='modal-header'>
                                     <h6 className='modal-title'>Create post</h6>
-                                    <a className='btn-close-canvas shadow onClosecreatepost' data-bs-dismiss='modal' aria-label='Close'><i className='bi-x'></i></a>
+                                    <a className='btn-close-canvas shadow onClosecreatepost' data-dismiss='modal' aria-label='Close'><i className='bi-x'></i></a>
                                 </div>
 
                                 <div className='modal-body'>
