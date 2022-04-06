d2672ccf22fdca781793de2e46d1a73ffcd3a296
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-06 13:18:20 +0800

    Update profile_page.tsx

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -582,12 +582,12 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                             <div className='col-md-5'><p className='mt-1'><strong>Add to your post</strong></p></div>
                                             <div className='col-md-7'>
                                                 <div className='btn-group float-end gap-2' role='group' aria-label='Add to your post group'>
-                                                    <a className='onTag ml-1'><img src={AttachIcon} /></a>
-                                                    <a className='onAddimage ml-1'><img src={ImgIcon} /></a>
-                                                    <a className='onAddfeelings ml-1'><img src={SmileIcon} /></a>
-                                                    <a className='onAddmusic ml-1'><img src={MusicIcon} /></a>
-                                                    <a className='onAddimage ml-1'><img src={VideoIcon} /></a>
-                                                    <a className='onLocation ml-1'><img src={GeoIcon} /></a>
+                                                    <a className='onTag ml-2'><img src={AttachIcon} height="20" width="20"/></a>
+                                                    <a className='onAddimage ml-2'><img src={ImgIcon} height="20" width="20" /></a>
+                                                    <a className='onAddfeelings ml-2'><img src={SmileIcon} height="20" width="20" /></a>
+                                                    <a className='onAddmusic ml-2'><img src={MusicIcon} height="20" width="20" /></a>
+                                                    <a className='onAddimage ml-2'><img src={VideoIcon} height="20" width="20" /></a>
+                                                    <a className='onLocation ml-2'><img src={GeoIcon} height="20" width="20" /></a>
                                                 </div>
                                             </div>
                                         </div>
