a37b3d8b7b02d83bd06031954c9fffbded221dc5
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-06 10:49:52 +0800

    Update app.jsx

--- components/app.jsx
+++ components/app.jsx
@@ -5,7 +5,7 @@ import {hot} from 'react-hot-loader/root';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {Router, Route} from 'react-router-dom';
