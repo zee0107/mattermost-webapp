270081696e0995befb4d8727a0c9cad1375b1284
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-06 13:44:22 +0800

    Update profile_page.tsx

--- components/profile_page/profile_page.tsx
+++ components/profile_page/profile_page.tsx
@@ -537,15 +537,9 @@ export default class ProfilPage extends React.PureComponent<Props, State> {
                                                 <a href='#' className='activities text-primary'><small className='text-muted'>Activities</small> &#128151;</a> 
                                             </strong>
                                             <br />
-                                            <a className='onSelectactionfriends text-primary'><i className='bi-people-fill'></i> Friends <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                                                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                                            </svg></a>
-                                            <a className='onSelectactionpublic text-primary'><i className='bi-globe'></i> Everyone <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                                                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                                            </svg></a>
-                                            <a className='onSelectactiononlyme text-primary'><i className='bi-person'></i> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                                                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                                            </svg></a>
+                                            <a className='onSelectactionfriends text-primary'><i className='bi-people-fill'></i> Friends <i className='bi-chevron-down'></i></a>
+                                            <a className='onSelectactionpublic text-primary'><i className='bi-globe'></i> Everyone <i className='bi-chevron-down'></i></a>
+                                            <a className='onSelectactiononlyme text-primary'><i className='bi-person'></i> Private <i className='bi-chevron-down'></i></a>
                                         </div>
                                     </div>
                                     <div className='row'>
