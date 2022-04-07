7cdc012d7f6edda9f0c5d02b7dc45cc5c52e9f11
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-07 14:46:22 +0800

    Update index.ts

--- components/create_post_profile/index.ts
+++ components/create_post_profile/index.ts
@@ -73,7 +73,7 @@ function makeMapStateToProps() {
         const license = getLicense(state);
         let data;
         let currentValue = Client4.getChannel(channelId);    
-        Promise.resolve(currentValue).then(value => {data=value;})    
+        Promise.resolve(currentValue).then(value => {data=value; console.log(data)});    
         console.log(data);
         const currentChannel = Client4.getChannel(channelId);
         const currentChannelTeammateUsername = getUser(state, currentChannel.teammate_id || '')?.username;
