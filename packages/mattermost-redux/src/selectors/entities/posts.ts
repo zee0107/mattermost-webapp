79e6b547821e6602e029e1ff3d5c59bb840826f0
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-12 15:56:22 +0800

    Updated

--- packages/mattermost-redux/src/selectors/entities/posts.ts
+++ packages/mattermost-redux/src/selectors/entities/posts.ts
@@ -463,7 +463,7 @@ export function makeGetPostsForIds(): (state: GlobalState, postIds: Array<Post['
                 return [];
             }
 
-            return postIds.map((id) => allPosts[id]).sort();
+            return postIds.map((id) => allPosts[id]);
         },
     );
 }
