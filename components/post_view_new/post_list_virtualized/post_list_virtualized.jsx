177f5df787d464f433b33e15805511ee88c1b88a
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-12 14:48:56 +0800

    Updated

--- components/post_view_new/post_list_virtualized/post_list_virtualized.jsx
+++ components/post_view_new/post_list_virtualized/post_list_virtualized.jsx
@@ -565,7 +565,7 @@ export default class PostList extends React.PureComponent {
             <div
                 role='list'
                 className='a11y__region'
-                data-a11y-sort-order='1'
+                data-a11y-sort-order='2'
                 data-a11y-focus-child={true}
                 data-a11y-order-reversed={false}
                 data-a11y-loop-navigation={false}
