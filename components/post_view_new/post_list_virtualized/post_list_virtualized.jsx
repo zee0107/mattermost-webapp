f6164ad6b68513e3bd2642c20077d81c0a540a95
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-13 15:47:32 +0800

    Update post_list_virtualized.jsx

--- components/post_view_new/post_list_virtualized/post_list_virtualized.jsx
+++ components/post_view_new/post_list_virtualized/post_list_virtualized.jsx
@@ -319,6 +319,8 @@ export default class PostList extends React.PureComponent {
         }
 
         // Since the first in the list is the latest message
+        const lastIndex = this.state.postListIds.length-1;
+        console.log(lastIndex);
         const isLastPost = itemId === this.state.postListIds[0];
 
         return (
