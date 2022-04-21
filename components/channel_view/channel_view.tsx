53e7ac6cfb16b8e0a0f99e3189a2193223a97fcb
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-21 16:25:50 +0800

    Update channel_view.tsx

--- components/channel_view/channel_view.tsx
+++ components/channel_view/channel_view.tsx
@@ -495,11 +495,9 @@ export default class ChannelView extends React.PureComponent<Props, State> {
             );
         }else{
             viewDetail = (
-                <div className='col-md-12 chat-box'>
-                    <GroupHeader
+                <GroupHeader
                         channelId={channelId}
                         channelDisplayName={channelDisplayName} />
-                </div>
             );
         }
 
@@ -534,7 +532,7 @@ export default class ChannelView extends React.PureComponent<Props, State> {
                                     </div>
                                     <div className='col-md-3'><div className='d-grid'>
                                         {shareInfoBtn}</div></div>
-                                        <div className='col-md-2'><div className='d-grid'><button className='box-live-post btn-sm width-100 p-3' onClick={() => { this.setState({uploading: true});}} aria-describedby='basic-addon1010' data-toggle='modal' data-target='#staticBackdrop'><svg xmlns='http://www.w3.org/2000/svg' width='19' height='19' fill='var(--text-primary)' className='bi bi-camera-video side-menu-align' viewBox='0 0 16 16'>
+                                        <div className='col-md-2'><div className='d-grid'><button className='box-live-post btn-sm width-100 p-3' onClick={() => { this.setState({uploading: true});}} aria-describedby='basic-addon1010' data-toggle='modal' data-target='#staticBackdrop'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='var(--text-primary)' className='bi bi-camera-video side-menu-align' viewBox='0 0 16 16'>
                                                 <path fillRule='evenodd' d='M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z' fill='var(--text-primary)'/>
                                                 </svg> Live</button></div>
                                         </div>
