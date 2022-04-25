a8890a8da73f242cbbbe9a9bed02187066957673
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-25 17:25:50 +0800

    Update index.ts

--- components/post_view_all/post/index.ts
+++ components/post_view_all/post/index.ts
@@ -52,7 +52,7 @@ function makeMapStateToProps() {
 
     return (state: GlobalState, ownProps: OwnProps) => {
         const post = ownProps.post || getPost(state, ownProps.postId);
-        console.log('Post: ',ownProps.postId);
+        console.log('Post: ',ownProps.post);
         const postDetailed = Client4.getPostDetailed(ownProps.postId);
         const channel = getChannel(state, post.channel_id);
 
