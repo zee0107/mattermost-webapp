f88f235eb8965a3b457bbc6b7a3cd76ba1a74a9f
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-25 14:04:36 +0800

    Update right_controls_style.tsx

--- components/global_header_style/right_controls/right_controls_style.tsx
+++ components/global_header_style/right_controls/right_controls_style.tsx
@@ -53,7 +53,7 @@ const RightControlsStyle = ({productId = null}: Props): JSX.Element => {
                 />
             )*/}
             
-            <a href="/messages" className="bg-icons"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-person-plus menu-align" viewBox="0 0 16 16">
+            <a href="/messages" className="bg-icons" data-toggle='modal' data-target='offcanvasRightLabelfriendsdesktop'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-person-plus menu-align" viewBox="0 0 16 16">
                 <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                 <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
             </svg></a>
@@ -72,6 +72,65 @@ const RightControlsStyle = ({productId = null}: Props): JSX.Element => {
             <path fillRule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" fill='var(--text-primary)'/>
             <path fillRule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" fill='var(--text-primary)'/>
             </svg> Logout</button>
+
+            <div style=' z-index: 8;' className='offcanvas offcanvas-end shadow-lg' data-bs-scroll='true' data-bs-backdrop='false' tabindex='-2' id='offcanvasRightLabelfriendsdesktop' aria-labelledby='offcanvasRightLabelfriendsdesktop'>
+                <div className='chat-list-indexes'>
+                <div className='offcanvas-header'>
+                    <h6 id='offcanvasRightLabelfriendsdesktop'>
+                        <i className='bi-person-plus-fill'></i> Friends</h6>
+                        <a className='btn-close-canvas shadow' data-dismiss='offcanvas' aria-label='Close'><i className='bi-x'></i></a>
+                </div>
+                <div className='offcanvas-body'>
+                    <div className='list-group mt-3 mb-3'>
+                        <strong>Friend request</strong>
+                        <div className='list-group-item list-group-item-action border-0 friends-contents'>
+                            <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
+                                <label className='mb-0'><img width='40' className='img-fluid user-photo' src='assets/images/sample-user-primary-picture-6.png' alt='User name' title='Username'/> Firstname</label>
+                                <label className='mt-2 approve-reject-text'><a className='approveActions'>Confirm</a> | <a className='rejeectActions'>Delete</a></label>
+                                <label className='mt-2 approve-text'><i className='bi-check-lg'></i> Approve</label>
+                                <label className='mt-2 reject-text'><a className='reject-actions'><i className='bi-x-lg'></i> Delete</a></label>
+                            </div>
+                        </div>
+                        <div className='list-group-item list-group-item-action border-0'>
+                            <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
+                                <label className='mb-0'><img width='40' className='img-fluid user-photo' src='assets/images/sample-user-primary-picture-5.png' alt='User name' title='Username'/> Firstname</label>
+                                <label className='mt-2'>Confirm | Delete</label>
+                            </div>
+                        </div>
+                        <div className='list-group-item list-group-item-action border-0'>
+                            <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
+                                <label className='mb-0'><img width='40' className='img-fluid user-photo' src='assets/images/sample-user-primary-picture-4.png' alt='User name' title='Username'/> Firstname</label>
+                                <label className='mt-2'>Confirm | Delete</label>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div className='list-group mt-3 mb-3'>
+                        <strong>People You May Know</strong>
+                        <div className='list-group-item list-group-item-action border-0 add-friends-contents'>
+                            <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
+                                <label className='mb-0'><img width='40' className='img-fluid user-photo' src='assets/images/sample-user-primary-picture.png' alt='User name' title='Username'/> Firstname</label>
+                                <label className='mt-2 addfriend-remove-text'><a className='addfriendActions'>Add Friend</a> | <a className='removeActions'>Remove</a></label>
+                                <label className='mt-2 addfriend-text'><i className='bi-check-lg'></i> Add Friend Request Sent</label>
+                                <label className='mt-2 remove-text'><a className='removehideActions'><i className='bi-x-lg'></i> Remove</a></label>
+                            </div>
+                        </div>
+                        <div className='list-group-item list-group-item-action border-0'>
+                            <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
+                                <label className='mb-0'><img width='40' className='img-fluid user-photo' src='assets/images/sample-user-primary-picture-3.png' alt='User name' title='Username'/> Firstname</label>
+                                <label className='mt-2'>Add Friend | Remove</label>
+                            </div>
+                        </div>
+                        <div className='list-group-item list-group-item-action border-0'>
+                            <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
+                                <label className='mb-0'><img width='40' className='img-fluid user-photo' src='assets/images/sample-user-primary-picture-2.png' alt='User name' title='Username'/> Firstname</label>
+                                <label className='mt-2'>Add Friend | Remove</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
         </RightControlsContainer>
     );
 };
