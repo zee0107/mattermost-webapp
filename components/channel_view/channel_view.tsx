4797212c80b4957be8d29fe3835ba013c0266ee9
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-25 16:42:05 +0800

    Update channel_view.tsx

--- components/channel_view/channel_view.tsx
+++ components/channel_view/channel_view.tsx
@@ -15,7 +15,7 @@ import ChannelHeader from 'components/channel_header';
 import CreatePost from 'components/create_post_all';
 import FileUploadOverlay from 'components/file_upload_overlay';
 import PostView from 'components/post_view_all';
-import Post from 'components/post_view_all/post';
+import Post from 'components/post_view_new/post';
 import {clearMarks, mark, measure, trackEvent} from 'actions/telemetry_actions.jsx';
 import FormattedMarkdownMessage from 'components/formatted_markdown_message';
 
