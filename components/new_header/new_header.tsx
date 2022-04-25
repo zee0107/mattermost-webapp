4ba64eb97d935cea771591a6c2474a46002a4acb
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-04-25 16:18:25 +0800

    Update new_header.tsx

--- components/new_header/new_header.tsx
+++ components/new_header/new_header.tsx
@@ -146,7 +146,7 @@ export default class NewHeader extends React.PureComponent<Props, State> {
                             </div>
                             <div className='text-end'>
                                 <div className='dropdown float-start'>
-                                    <a className='btn-add-post text-white' id='dropdownCreateposts' data-toggle='dropdown' aria-expanded='true'>
+                                    <a className='btn-add-post text-white' id='dropdownCreateposts' data-bs-toggle='dropdown' aria-expanded='true'>
                                     <label className='plus-text-sizes'>+</label></a>
 
                                     <ul className='dropdown-menu' aria-labelledby='dropdownCreateposts'>
@@ -157,21 +157,21 @@ export default class NewHeader extends React.PureComponent<Props, State> {
                                         <li><a className='dropdown-item' href='#'><img width='19' className='img-fluid me-2' src='assets/images/icon-create-page.png'/> Create Page</a></li>
                                     </ul>
                                 </div>
-                                <a className='position-relative onDekstopaddfriends' data-toggle='offcanvas' data-bs-target='#offcanvasRightLabelfriendsdesktop' aria-controls='offcanvasRightLabelfriendsdesktop'>
+                                <a className='position-relative onDekstopaddfriends' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRightLabelfriendsdesktop' aria-controls='offcanvasRightLabelfriendsdesktop'>
                                     <svg xmlns='http://www.w3.org/2000/svg' fill='var(--text-primary)' className='bi bi-person-plus menu-align bi-person-plus-style' viewBox='0 0 16 16'><path d='M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z'></path><path fillRule='evenodd' d='M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z'></path></svg>
                                     {/*<span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger badge-style'>3</span>*/}
                                 </a>
-                                <a className='position-relative onDekstopaddmessages' data-toggle='offcanvas' data-bs-target='#offcanvasRightLabelchatdesktop' aria-controls='offcanvasRightLabelchatdesktop'>
+                                <a className='position-relative onDekstopaddmessages' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRightLabelchatdesktop' aria-controls='offcanvasRightLabelchatdesktop'>
                                     <svg xmlns='http://www.w3.org/2000/svg' fill='var(--text-primary)' className='bi bi-chat-right-text menu-align bi-chat-right-text-style' viewBox='0 0 16 16'><path d='M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z'></path><path d='M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z'></path></svg>
                                     {/*<span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger badge-style'>10</span>*/}
                                 </a>
-                                <a className='position-relative onDekstopaddnotifications' data-toggle='offcanvas' data-bs-target='#offcanvasRightnotificationdesktop' aria-controls='offcanvasRightnotificationdesktop'>
+                                <a className='position-relative onDekstopaddnotifications' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRightnotificationdesktop' aria-controls='offcanvasRightnotificationdesktop'>
                                     <svg xmlns='http://www.w3.org/2000/svg' fill='var(--text-primary)' className='bi bi-bell menu-align bi-bell-styles' viewBox='0 0 16 16'><path d='M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z'></path></svg>
                                     {/*<span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger badge-style'>5</span>*/}
                                 </a>
                             </div>
                             <div className='dropdown'>
-                                    <button className='btn-crypter-user' type='button' id='dropdownMenuButton1' data-toggle='dropdown' aria-expanded='false'>
+                                    <button className='btn-crypter-user' type='button' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>
                                     {this.renderProfilePicture('md')} Crypter User <i className='bi-chevron-down'></i>
                                     </button>
                                     <ul className='dropdown-menu dropdown-menu-dark mt-1' aria-labelledby='dropdownMenuButton1'>
@@ -191,7 +191,7 @@ export default class NewHeader extends React.PureComponent<Props, State> {
                     </div>
                 </header>
             </div>
-            <div style={{ zIndex: 8,}} className='offcanvas offcanvas-end shadow-lg' data-scroll='true' data-backdrop='false' tabIndex='-1' id='offcanvasRightLabelfriendsdesktop' aria-labelledby='offcanvasRightLabelfriendsdesktop'>
+            <div style={{ zIndex: 8,}} className='offcanvas offcanvas-end shadow-lg' data-bs-scroll='true' data-bs-backdrop='false' tabIndex='-2' id='offcanvasRightLabelfriendsdesktop' aria-labelledby='offcanvasRightLabelfriendsdesktop'>
                 <div className='chat-list-indexes'>
                     <div className='offcanvas-header'>
                         <h6 id='offcanvasRightLabelfriendsdesktop'>
