4a489bb62b13c3729ad1ef8a4e6839fe9fa9d28a
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-04 16:42:32 +0800

    Update button_connect.js

--- components/create_token/button_connect.js
+++ components/create_token/button_connect.js
@@ -1,5 +1,5 @@
 import { useWeb3React } from "@web3-react/core";
-import { connectors } from "./connectors";
+import { connectors } from "../connectors";
 import { toHex, truncateAddress } from "../wallet_utils";
 
 export default function ButtonConnect(){
