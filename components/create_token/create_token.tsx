a3fce5123e1c076e11c2bbb75fca3d83cca48709
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-06 11:25:10 +0800

    Update create_token.tsx

--- components/create_token/create_token.tsx
+++ components/create_token/create_token.tsx
@@ -77,13 +77,13 @@ export default class CreateToken extends React.PureComponent<Props, State> {
         this.handleSymbolChange = this.handleSymbolChange.bind(this);
     }
 
-    componentDidMount = () =>{
+    componentDidMount = async () =>{
         const ThemeValue = window.localStorage.getItem("theme");
         this.setState({isDark: ThemeValue});
 
-        /*const web3Info = web3();
-        const accounts = web3Info.user.eth.getAccounts();
-        console.log(accounts);*/
+        const web3Info = await web3();
+        const accounts = await web3Info.user.eth.getAccounts();
+        console.log(accounts);
         const savedNetwork = window.localStorage.getItem('chainNetwork');
         if(savedNetwork !== undefined && savedNetwork !== null && savedNetwork !== '')
         {
