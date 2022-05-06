920164c10908c0a1beea439790ec56cafca68895
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-06 11:00:54 +0800

    Update create_token.tsx

--- components/create_token/create_token.tsx
+++ components/create_token/create_token.tsx
@@ -9,7 +9,7 @@ import SelectWalletModal from "./Modal";
 import ButtonConnect from "./button_connect";
 import NetworkModal from "./network_modal";
 
-//import web3 from 'web3';
+import web3 from 'web3';
 
 import { useWeb3React } from "@web3-react/core";
 import { networkParams } from "../networks";
