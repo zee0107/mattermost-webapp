286ba93ee1e6043690a3cbda11c71cd921c275cb
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-11 13:23:59 +0800

    Update right_controls_style.tsx

--- components/global_header_style/right_controls/right_controls_style.tsx
+++ components/global_header_style/right_controls/right_controls_style.tsx
@@ -53,7 +53,18 @@ const RightControlsStyle = ({productId = null}: Props): JSX.Element => {
                         pluggableId={productId}
                     />
                 )*/}
-                
+                <div className='dropdown float-start'>
+                    <a className='btn-add-post text-white' id='dropdownCreateposts' data-bs-toggle='dropdown' aria-expanded='true'>
+                    <label className='plus-text-sizes'>+</label></a>
+
+                    <ul className='dropdown-menu' aria-labelledby='dropdownCreateposts'>
+                        <li><a className='dropdown-item' href='#'><img width='19' className='img-fluid mr-2' src='assets/images/arrow-create-ad.png'/> Create AD</a></li>
+                        <li><a className='dropdown-item' href='#'><img width='19' className='img-fluid mr-2' src='assets/images/icon-create-blog.png'/> Create Blog</a></li>
+                        <li><a className='dropdown-item' href='#'><img width='19' className='img-fluid mr-2' src='assets/images/icon-create-event.png'/> Create Event</a></li>
+                        <li><a className='dropdown-item' href='#'><img width='19' className='img-fluid mr-2' src='assets/images/icon-create-group.png'/> Create Group</a></li>
+                        <li><a className='dropdown-item' href='#'><img width='19' className='img-fluid mr-2' src='assets/images/icon-create-page.png'/> Create Page</a></li>
+                    </ul>
+                </div>
                 <a className="bg-icons" data-toggle='offcanvas' data-target='#offcanvasRightLabelfriendsdesktop' aria-controls='offcanvasRightLabelfriendsdesktop'><svg data-toggle='offcanvas' data-target='#offcanvasRightLabelfriendsdesktop'  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--text-primary)" className="bi bi-person-plus menu-align" viewBox="0 0 16 16">
                     <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                     <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
