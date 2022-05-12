1b92d9886fedf7b8143a2b43b6239b78daa77f8f
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-12 17:11:53 +0800

    Updated QA

--- components/create_post_profile/create_post_profile.tsx
+++ components/create_post_profile/create_post_profile.tsx
@@ -383,8 +383,8 @@ class CreatePostProfile extends React.PureComponent<Props, State> {
             errorClass: null,
             serverError: null,
             currentChannel: {},
-            //channelId: 'kqe4sihhdid47gprhk6dwbuc4o',
-            channelId: 'dodurztr1fbupnpenjgxqjso3a',
+            channelId: 'kqe4sihhdid47gprhk6dwbuc4o',
+            //channelId: 'dodurztr1fbupnpenjgxqjso3a',
             uploading: false,
         };
 
@@ -416,8 +416,8 @@ class CreatePostProfile extends React.PureComponent<Props, State> {
         if (useGroupMentions) {
             actions.getChannelMemberCountsByGroup(this.state.currentChannel.id, isTimezoneEnabled);
         }
-        //this.setState({channelId: 'kqe4sihhdid47gprhk6dwbuc4o'});
-        this.setState({channelId: 'dodurztr1fbupnpenjgxqjso3a'});
+        this.setState({channelId: 'kqe4sihhdid47gprhk6dwbuc4o'});
+        //this.setState({channelId: 'dodurztr1fbupnpenjgxqjso3a'});
     }
 
     componentDidUpdate(prevProps: Props, prevState: State) {
@@ -452,8 +452,8 @@ class CreatePostProfile extends React.PureComponent<Props, State> {
         }
 
         if (prevState.channelId !== this.state.channelId){
-            //this.setState({channelId: 'kqe4sihhdid47gprhk6dwbuc4o'});
-            this.setState({channelId: 'dodurztr1fbupnpenjgxqjso3a'});
+            this.setState({channelId: 'kqe4sihhdid47gprhk6dwbuc4o'});
+            //this.setState({channelId: 'dodurztr1fbupnpenjgxqjso3a'});
         }
     }
 
