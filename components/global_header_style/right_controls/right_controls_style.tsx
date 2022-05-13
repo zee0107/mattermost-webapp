34ef2204af7c3750742912953542be448833c137
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-13 11:07:33 +0800

    Update right_controls_style.tsx

--- components/global_header_style/right_controls/right_controls_style.tsx
+++ components/global_header_style/right_controls/right_controls_style.tsx
@@ -55,10 +55,8 @@ async function getUserList(){
     var data = await Client4.getChannelMembers('kqe4sihhdid47gprhk6dwbuc4o');
     return data;
 }
-const RightControlsStyle = async ({productId = null}: Props): JSX.Element => {
+const RightControlsStyle = ({productId = null}: Props): JSX.Element => {
     const showSettingsTip = useShowTutorialStep(TutorialSteps.SETTINGS);
-    const profiles = await getUserList();
-    console.log('Profiles: ', profiles);
     
     return (
         <>
