f5aec2f8f9e03304836c8524f5d16798a1263777
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-13 15:47:34 +0800

    Update request_list.tsx

--- components/request_list/request_list.tsx
+++ components/request_list/request_list.tsx
@@ -255,7 +255,7 @@ export default class RequestLists extends React.PureComponent<Props, State> {
                 <div className='list-group-item list-group-item-action border-0 friends-contents'>
                     <div className='d-flex w-100 justify-content-between mt-1 mb-1'>
                         <label className='mb-0'>{this.renderProfilePictureText('mxl')} test</label>
-                        <label className='mt-2 approve-reject-text'><a className='approveActions' onClick={handleAccept}>Confirm</a> | <a className='rejeectActions'>Delete</a></label>
+                        <label className='mt-2 approve-reject-text'><a className='approveActions' onClick={this.handleAccept}>Confirm</a> | <a className='rejeectActions'>Delete</a></label>
                         <label className='mt-2 reject-text'><a className='reject-actions' onClick={this.handleCancelRequest}><i className='bi-x-lg'></i> Delete</a></label>
                     </div>
                 </div>
