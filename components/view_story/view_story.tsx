d88c7287d755ad0f45d117d777c141890b67af88
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-20 16:40:11 +0800

    Updated

--- components/view_story/view_story.tsx
+++ components/view_story/view_story.tsx
@@ -169,71 +169,93 @@ export default class ViewStory extends React.PureComponent<Props, State> {
                                         <br/>
                                         <strong><label>Select a story to open</label></strong>
                                         </p>
+
                                         {/*Previews all story*/}
-                                        <div className='create-all-stories-previews'>
-                                            <div className='row'>
-                                                <div className='col-8'>
-                                                    <img className='img-fluid float-none' src='assets/images/sample-user-primary-picture-5.png'/>
-                                                    <small className='float-none ms-2 text-muted'><strong>Firstname 8m</strong> <i className='bi-people-fill'></i></small>
-                                                </div>
-                                                <div className='col-4'>
-                                                    <div className='dropdown'>
+                                        <div className='row p-3'>
+                                            <div className='create-all-stories-previews'>
+                                                <div className='row'>
+                                                    <div className='col-8'>
+                                                        <img className='img-fluid float-none' src='assets/images/sample-user-primary-picture-5.png'/>
+                                                        <small className='float-none ms-2 text-muted'><strong>Firstname 8m</strong> <i className='bi-people-fill'></i></small>
+                                                    </div>
+                                                    <div className='col-4'>
+                                                        <div className='dropdown'>
                                                         <a className='onClosestoryallpreviewsactions float-end shadow ms-1' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'><i className='bi-three-dots-vertical'></i></a>
                                                         <a className='onClosestoryallpreviews float-end shadow ms-1'><i className='bi-x-lg'></i></a>
-
-                                                        <ul className='dropdown-menu dropdown-menu-dark' aria-labelledby='dropdownMenuButton1'>
-                                                            <li><a className='dropdown-item'><i className='bi-x-octagon-fill'></i> Mute Firstname goes here</a></li>
-                                                            <li><a className='dropdown-item'><i className='bi-patch-exclamation-fill'></i> Find support or report story</a></li>
-                                                        </ul>
                                                     </div>
+
+                                                    <ul className='dropdown-menu dropdown-menu-dark' aria-labelledby='dropdownMenuButton1'>
+                                                        <li><a className='dropdown-item'><i className='bi-x-octagon-fill'></i> Mute Firstname goes here</a></li>
+                                                        <li><a className='dropdown-item'><i className='bi-patch-exclamation-fill'></i> Find support or report story</a></li>
+                                                    </ul>
                                                 </div>
                                             </div>
-
-                                            <div className='previews-content mt-3 mb-3 text-center'>
-                                                <strong className='text-center text-white'><label>CREATED STORIES FROM YOU GOES HERE</label></strong>
-                                            </div>
-
-                                            {/*Previews content actions*/}
-                                            <div className='previews-content-actions'>
-                                                <div className='row'>
-                                                    <div className='col-lg-9'>
-                                                        <div className='form-floating'>
-                                                            <textarea className='form-control textares-stories-input' placeholder='Send messages...' id=''></textarea>
-                                                            <label htmlFor='floatingTextarea'>Send messages...</label>
-                                                        </div>
-                                                        <div className='position-relative float-end'>
-                                                            <a className='onSendmessage'>
-                                                                <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' className='bi bi-send-fill send-icons-previews' viewBox='0 0 16 16'>
-                                                                <path d='M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z'/>
-                                                                </svg>
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                    <div className='col-lg-3 text-center'>
-                                                        <p>
-                                                            <a className='onClicklikepreviewstories position-relative me-3'><i className='bi-hand-thumbs-up style-hand-thumbs-up-fill'></i>
-                                                            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger count-likes-previews'>
-                                                                2m+
-                                                            </span>
-                                                            </a>
-                                                            <a className='onClickheartpreviewstories position-relative'><i className='bi-heart style-heart-fill'></i>
-                                                            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger count-heart-previews'>
-                                                                3.5k+
-                                                            </span>
-                                                            </a>
-
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            {/*Previews content actions*/}
                                         </div>
-                                        {/*Previews all story*/}
+                                        <div className='previews-content mt-3 mb-3 text-center'>
+                                            <div id='carouselStoryloopIndicators' className='carousel slide' data-bs-ride='carousel'>
+                                                <div className='carousel-indicators'>
+                                                    <button type='button' data-bs-target='#carouselStoryloopIndicators' data-bs-slide-to='0' className='active' aria-current='true' aria-label='Slide 1'></button>
+                                                    <button type='button' data-bs-target='#carouselStoryloopIndicators' data-bs-slide-to='1' aria-label='Slide 2'></button>
+                                                    <button type='button' data-bs-target='#carouselStoryloopIndicators' data-bs-slide-to='2' aria-label='Slide 3'></button>
+                                                </div>
+                                                {/*Loop for many story post*/}
+                                                <div className='carousel-inner text-center'>
+                                                    <div className='carousel-item active'>
+                                                    <img width='' src='assets/images/Cover-album-2.jpg' className='d-block w-100' alt=''/>
+                                                    </div>
+                                                    <div className='carousel-item'>
+                                                    <img src='assets/images/Cover-album-3.jpg' className='d-block w-100' alt=''/>
+                                                    </div>
+                                                    <div className='carousel-item'>
+                                                    <img src='assets/images/Cover-album.jpg' className='d-block w-100' alt=''/>
+                                                    </div>
+                                                </div>
+                                                {/*Loop for many story post*/}
+                                                <button className='carousel-control-prev' type='button' data-bs-target='#carouselStoryloopIndicators' data-bs-slide='prev'>
+                                                <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                                                <span className='visually-hidden'>Previous</span>
+                                                </button>
+                                                <button className='carousel-control-next' type='button' data-bs-target='#carouselStoryloopIndicators' data-bs-slide='next'>
+                                                <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                                                <span className='visually-hidden'>Next</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div className='previews-content-actions'>
+                                            <div className='row'>
+                                                <div className='col-lg-9'>
+                                                    <div className='form-floating'>
+                                                    <textarea className='form-control textares-stories-input' placeholder='Send messages...' id=''></textarea>
+                                                    <label htmlFor='floatingTextarea'>Send messages...</label>
+                                                    </div>
+                                                    <div className='position-relative float-end'>
+                                                    <a className='onSendmessage'>
+                                                        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' className='bi bi-send-fill send-icons-previews' viewBox='0 0 16 16'>
+                                                        <path d='M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z'/>
+                                                        </svg>
+                                                    </a>
+                                                    </div>
+                                                </div>
+                                                <div className='col-lg-3 text-center'>
+                                                <p>
+                                                    <a className='onClicklikepreviewstories position-relative me-3'><i className='bi-hand-thumbs-up style-hand-thumbs-up-fill'></i>
+                                                    <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger count-likes-previews'>
+                                                        2m+
+                                                    </span>
+                                                    </a>
+                                                    <a className='onClickheartpreviewstories position-relative'><i className='bi-heart style-heart-fill'></i>
+                                                    <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger count-heart-previews'>
+                                                        3.5k+
+                                                    </span>
+                                                    </a>
+                                                </p>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
+                                </div>
 
                                     <div className='row p-3'>
-                                        
-
                                         <div className='all-stories-float-icon-desktop'>
                                             <div className='position-absolute top-0 end-0 mt-4 me-4'>
                                                {/* <a className='onStorynotifications' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRightnotificationdesktop' aria-controls='offcanvasRightnotificationdesktop'><i className='bi-bell-fill'></i></a>
