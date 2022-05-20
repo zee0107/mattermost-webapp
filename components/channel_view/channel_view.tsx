b7500e1cd367a28f54e2b53c629f0f8c538b6bb2
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-20 15:36:20 +0800

    Update channel_view.tsx

--- components/channel_view/channel_view.tsx
+++ components/channel_view/channel_view.tsx
@@ -471,9 +471,7 @@ export default class ChannelView extends React.PureComponent<Props, State> {
                                         {this.renderProfilePicture('xl')}
                                     </a>
                                 </div>
-                                <div className='badges-online-plus rounded-circle onClickstory position-relative'><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="#fff" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-                                </svg></div>
+                                <div className='badges-online-plus rounded-circle onClickstory position-relative text-center'><i className='bi bi-plus'></i></div>
                                 <small className='firstname-title-story'>Your story</small>
                             </div>
                             {storyListDetails}
