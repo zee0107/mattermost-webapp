47356c97c4e1f7996187ddd631641424fbac0e41
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-24 15:17:18 +0800

    Update view_story.tsx

--- components/view_story/view_story.tsx
+++ components/view_story/view_story.tsx
@@ -86,10 +86,6 @@ export default class ViewStory extends React.PureComponent<Props, State> {
     }
 
     componentDidUpdate(_,prevState){
-        if(this.state.privacyValue !== prevState.privacyValue){
-            this.getUserSettings();
-        }
-
         if(this.state.userSettings !== prevState.userSettings){
             this.setDefault(this.state.userSettings.story_privacy);
         }
