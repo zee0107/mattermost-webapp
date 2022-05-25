45ad4acfc2a603b26503081f33d4cf0ed52d5841
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-25 09:48:11 +0800

    Update create_album.tsx

--- components/create_album/create_album.tsx
+++ components/create_album/create_album.tsx
@@ -82,12 +82,13 @@ export default class CreateAlbum extends React.PureComponent<Props, State> {
             if(e.target.files.length > 0 ){
                 for(var i = 0; i <= e.target.files.length; i++){
                     console.log(e.target.files[i]);
-                }
-                /*for(var i = 0; i <= e.target.files.length; i++){
                     this.setState((prevState) => ({
                         photoValue: [...prevState.photoValue, e.target.files[i]],
                         photoValueName: [...prevState.photoValueName, e.target.files[i].name]
                     }));
+                }
+                /*for(var i = 0; i <= e.target.files.length; i++){
+                   
                 }*/
             }
         } else {
