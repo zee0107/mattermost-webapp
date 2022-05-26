2f91188dea5f242d557553f83c4eb4c3cff865ab
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-26 16:17:24 +0800

    Update album_view.tsx

--- components/album_view/album_view.tsx
+++ components/album_view/album_view.tsx
@@ -109,30 +109,30 @@ export default class AlbumView extends React.PureComponent<Props, State> {
                                 );
                             })}
                         </div>*/}
-                        <div className='col-md-12'>
-                            <div className='carousel-inner text-center'>
-                                {imageList.map((item,index) => {
-                                    let activeClass;
-                                    if(index === 0){
-                                        activeClass = 'active';
-                                    }
                                     return (
-                                        <div className={`carousel-item ${activeClass}`} key={`${item}-${index}`}>
-                                            <AlbumImage albumId={album.id} fileName={item} />
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                            <a className='carousel-control-prev position-absolute top-50 start-0 translate-middle' href='#' style={{width:'10%'}} data-bs-target='#carouselloopIndicators' data-bs-slide='prev' role='button'>
-                                <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-                                <span className='sr-only'>Previous</span>
-                            </a>
-                            <a className='carousel-control-next position-absolute top-50 start-100 translate-middle' href='#' style={{width:'10%'}} data-bs-target='#carouselloopIndicators' data-bs-slide='next' role='button'>
-                                <span className='carousel-control-next-icon' aria-hidden='true'></span>
-                                <span className='sr-only'>Next</span>
-                            </a>
+                        <div className='carousel-inner text-center'>
+                            {imageList.map((item,index) => {
+                                let activeClass;
+                                if(index === 0){
+                                    activeClass = 'active';
+                                }
+                                return (
+                                    <div className={`carousel-item ${activeClass}`} key={`${item}-${index}`}>
+                                        <AlbumImage albumId={album.id} fileName={item} />
+                                    </div>
+                                );
+                            })}
                         </div>
+                        
                     </div>
+                    <a className='carousel-control-prev position-absolute top-50 start-0 translate-middle' href='#' style={{width:'10%'}} data-bs-target='#carouselloopIndicators' data-bs-slide='prev' role='button'>
+                        <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                        <span className='sr-only'>Previous</span>
+                    </a>
+                    <a className='carousel-control-next position-absolute top-50 start-100 translate-middle' href='#' style={{width:'10%'}} data-bs-target='#carouselloopIndicators' data-bs-slide='next' role='button'>
+                        <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                        <span className='sr-only'>Next</span>
+                    </a>
                 </>
             );
         }
