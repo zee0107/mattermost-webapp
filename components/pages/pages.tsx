07783b79fd1617f295506977191b5f6187ea2bb1
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-05-27 16:47:48 +0800

    Update pages.tsx

--- components/pages/pages.tsx
+++ components/pages/pages.tsx
@@ -449,7 +449,6 @@ export default class MyPages extends React.PureComponent<Props, State> {
             </div> 
         );
     }
-
     suggestedGroup = () => {
         let errorServer;
         if (this.state.serverError) {
