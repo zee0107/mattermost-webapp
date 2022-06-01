926484506b91d24238f815be3a10d9553bf1eaf4
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-01 12:37:22 +0800

    Update forum-discussion.tsx

--- components/forum-discussion/forum-discussion.tsx
+++ components/forum-discussion/forum-discussion.tsx
@@ -244,68 +244,7 @@ export default class ForumDiscussion extends React.PureComponent<Props, State> {
                     </div>
                 </section>
                 <section className='crypter-section-mobile'>
-                    <div className='container-fluid'>
-                        <div className='position-sticky float-middle-panel'>
-                            <div className='box-middle-panel-marketplace-mobile bg-transparent mb-3'>
-                                <div className='row'>
-                                    <div className='col-6'>
-                                        <a className='onForum float-start'><i className='bi-chat-left-text-fill'></i></a>
-                                        <small className='ms-2 text-marketplace float-start mt-2'><strong>Forum</strong></small>
-                                    </div>
-                                    <div className='col-6 mt-1'>
-                                        <a className='float-end'><i className='bi-arrow-left-circle'></i></a>
-
-                                        <a className='float-end mt-0 me-2 bg-white p-2 rounded' data-bs-toggle='collapse' href='#collapseJumpto' role='button' aria-expanded='true' aria-controls='collapseJumpto'>
-                                        <strong><label>Jump to <i className='bi-chevron-down'></i></label></strong>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className='collapse mt-1' id='collapseJumpto'>
-                                    <ul className='list-group'>
-                                        <li className={`list-group-item border-0 onBrowse ${selectedMenu === 'browse' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'browse'}); }}>Browse</li>
-                                        <li className={`list-group-item border-0 onMemebers ${selectedMenu === 'members' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'members'}); }}>Members</li>
-                                        <li className={`list-group-item border-0 onSearch ${selectedMenu === 'search' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''} `} onClick={() => { this.setState({selectedMenu: 'search'}); }}>Search</li>
-                                        <li className={`list-group-item border-0 onMythreads ${selectedMenu === 'threads' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'threads'}); }}>My Threads</li>
-                                        <li className={`list-group-item border-0 onMymessages ${selectedMenu === 'messages' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'messages'}); }}>My Messages</li>
-                                        <li className={`list-group-item border-0 onMymessages`}><a href='/create-forum-thread'>Create a Topic</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='box-middle-panel-forums-mobile mb-3'>
-                            <div id='carouselForumsmenumobileControls' className='carousel slide' data-bs-ride='carouselforums'>
-                                <button className='carousel-control-prev' type='button' data-bs-target='#carouselForumsmenumobileControls' data-bs-slide='prev'>
-                                </button>
-                                <button className='carousel-control-next' type='button' data-bs-target='#carouselForumsmenumobileControls' data-bs-slide='next'>
-                                </button>
-                                <div className='carousel-inner'>
-                                    <div className='carousel-item active'>
-                                        <div className='row'>
-                                            <div className='col-4 text-center'><a className={`onBrowse forum-menu-links ${selectedMenu === 'browse' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'browse'}); }}><strong>Browse</strong></a></div>
-                                            <div className='col-4 text-center'><a className={`onMemebers forum-menu-links ${selectedMenu === 'members' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'members'}); }}><strong>Members</strong></a></div>
-                                            <div className='col-4 text-center'><a className={`onSearch forum-menu-links ${selectedMenu === 'search' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'search'}); }}><strong>Search</strong></a></div>
-                                        </div>
-                                    </div>
-                                    <div className='carousel-item'>
-                                        <div className='row'>
-                                            <div className='col-4 text-center'><a className={`onMythreads forum-menu-links ${selectedMenu === 'threads' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'threads'}); }}><strong>My Threads</strong></a></div>
-                                            <div className='col-4 text-center'><a className={`onMymessages forum-menu-links ${selectedMenu === 'messages' ? 'border-bottom border-success border-3 fw-bold forums-text-menu-color':''}`} onClick={() => { this.setState({selectedMenu: 'messages'}); }}><strong>My Messages</strong></a></div>
-                                            <div className='col-4 text-center'></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='container-fluid'>
-                        <div className='row'>
-                            <div className='col-md-12'>
-                                <div className='box-middle-panel-products-mobile'>
-                                    {renderViewMobile}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </section>
             </>
         );
