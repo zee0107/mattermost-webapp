43f03b771a7c8ce87f095cb61b316ab07f39fef7
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-02 17:21:08 +0800

    Update channel_view.tsx

--- components/channel_view/channel_view.tsx
+++ components/channel_view/channel_view.tsx
@@ -703,22 +703,22 @@ export default class ChannelView extends React.PureComponent<Props, State> {
                             <div className='col-md-12 profile-menu-box-mobile width-100'>
                                 <div className='d-flex'>
                                     <div className='col-lg-2 profile-menu-icon'>
-                                        <a href='#all' onClick={this.onChangeFilter('all')}><img src={LayoutIcon}></img></a>
+                                        <a href='#all' onClick={() => this.onChangeFilter('all')}><img src={LayoutIcon}></img></a>
                                     </div>
                                     <div className='col-lg-2 profile-menu-icon'>
-                                        <a href='#images' onClick={this.onChangeFilter('image')}><img src={ImgIcon}></img></a>
+                                        <a href='#images' onClick={() => this.onChangeFilter('image')}><img src={ImgIcon}></img></a>
                                     </div>
                                     <div className='col-lg-2 profile-menu-icon'>
-                                        <a href='#videos' onClick={this.onChangeFilter('video')}><img src={VideoIcon}></img></a>
+                                        <a href='#videos' onClick={() => this.onChangeFilter('video')}><img src={VideoIcon}></img></a>
                                     </div>
                                     <div className='col-lg-2 profile-menu-icon'>
-                                        <a href='#music' onClick={this.onChangeFilter('audio')}><img src={MusicIcon}></img></a>
+                                        <a href='#music' onClick={() => this.onChangeFilter('audio')}><img src={MusicIcon}></img></a>
                                     </div>
                                     <div className='col-lg-2 profile-menu-icon'>
-                                        <a href='#file' onClick={this.onChangeFilter('file')}><img src={AttachIcon}></img></a>
+                                        <a href='#file' onClick={() => this.onChangeFilter('file')}><img src={AttachIcon}></img></a>
                                     </div>
                                     <div className='col-lg-2 profile-menu-icon'>
-                                        <a href='#location' onClick={this.onChangeFilter('location')}><img src={GeoIcon}></img></a>
+                                        <a href='#location' onClick={() => this.onChangeFilter('location')}><img src={GeoIcon}></img></a>
                                     </div>
                                 </div>
                             </div>
