394d6a4864f52a75573f8c36c6702f1d6369347d
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-03 15:56:38 +0800

    Update status_dropdown.tsx

--- components/status_dropdown/status_dropdown.tsx
+++ components/status_dropdown/status_dropdown.tsx
@@ -499,16 +499,16 @@ export default class StatusDropdown extends React.PureComponent<Props, State> {
                             rightDecorator={status === 'forums' && selectedIndicator}
                             id={'forums'}
                         />
-                        {/*<Menu.ItemLink
-                            id='forums'
-                            ariaLabel='Forums'
-                            to='/forums'
+                        <Menu.ItemLink
+                            id='messages'
+                            ariaLabel='messages'
+                            to='/messages'
                             o
                             text={localizeMessage('navbar_dropdown', 'Join Forums')}
                             icon={globalHeader ? (
-                                <i className='bi bi-flag'/>}
+                                <i className='bi bi-messages-left'/>
                             ) : <i className='fa fa-cog'/>}
-                            />*/}
+                            />
                     </Menu.Group>
                     <Menu.Group>
                         <Menu.ItemLink
