48ee2a2ecb2c9fea9bf7913c45d597b178128093
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-03 17:32:26 +0800

    Updated QA

--- components/forum_comments/forum_comments.tsx
+++ components/forum_comments/forum_comments.tsx
@@ -23,6 +23,7 @@ type Props = {
 type State = {
     isDark: string;
     post: any;
+    editComment: string;
 };
 
 export default class ForumComments extends React.PureComponent<Props, State> {
@@ -30,7 +31,8 @@ export default class ForumComments extends React.PureComponent<Props, State> {
 
     constructor(props: Props) {
         super(props);
-        this.state = {isDark:'light',};
+        this.state = {isDark:'light',editComment: false};
+        
     }
 
     componentDidMount(){
@@ -40,6 +42,26 @@ export default class ForumComments extends React.PureComponent<Props, State> {
         if(this.props.post !== undefined && this.props.post !== null){
             Promise.resolve(this.props.post).then((value) => { this.setState({post: value}); });
         }
+    }
+
+    handleArchive = (id: string) => {
+        const {baseUri} = this.state;
+        const uri = new URL(baseUri + 'archivecomment');
+        const params = {comment_id: id};
+        uri.search = new URLSearchParams(params);
+
+        fetch(uri, {
+            method: 'POST',
+        }).then((response) => response.json()).then((data)=>{    
+            if(data === 'NotFound'){
+                this.setState({textError: 'Data not found.'});
+            }
+            this.setState({post: data});
+        }).catch(error => this.setState({textError: error}));
+    }
+
+    handleEditComment = (value: boolean) => {
+        this.setState({editComment: value});
     }
 
     onFullWidth = () => {
@@ -66,7 +88,7 @@ export default class ForumComments extends React.PureComponent<Props, State> {
 
     render= (): JSX.Element => {
         const {currentUser, userData, view, postType, fullWidth} = this.props;
-        const {post} = this.state;
+        const {post, editComment} = this.state;
         let name;
         if(currentUser){
             name = currentUser.username;
@@ -129,90 +151,80 @@ export default class ForumComments extends React.PureComponent<Props, State> {
             }
         }
         else{
+            let editCommentValue;
             let postArchive;
+            let postUser;
             if(post){
                 postDesc = post.comment;
                 postId = post.id;
                 postArchive = post.archive;
+                postUser = post.post_user;
             }
 
+            let dropdown;
+            if(currentUser.id === postUser){
+                dropdown = (
+                    <div className='position-relative'>
+                        <div className='dropdown'>
+                            <a className='position-absolute top-0 end-0 onActioncomment text-dark' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>
+                                <i className='bi-three-dots-vertical bi-layout-sidebar-inset-reverse-style'></i>
+                            </a>
+                            <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+                                <li><a className='dropdown-item onDeletecomment' onClick={() => this.handleArchive(postId)}>Delete</a></li>
+                                <li><a className='dropdown-item onEditcomments' onClick={() => this.handleEditComment(true)}>Edit comment</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                );
+            }
+
+            if(editComment){
+                editCommentValue = (
+                    <form className='p-2 editcomments'>
+                        <div className='form-floating'>
+                            <textarea className='form-control' placeholder='Edit comment' id='floatingTextarea2' style={{height: '100px',}}></textarea>
+                            <label htmlFor='floatingTextarea2'>
+                                Edit comment
+                            </label>
+                        </div>
+                        <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white'>
+                            Update
+                        </button>
+                        <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white onCanceledit' onClick={() => this.handleEditComment(false)}>
+                            Cancel
+                        </button>
+                        <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white onDeletecomment' onClick={() => this.handleArchive(postId)}>
+                            <i className='bi-trash-fill bi-trash-fill-style'></i>
+                        </button>
+                    </form>
+                );
+            }
             if(postArchive !== 1){
                 if(view === 'desktop'){
                     renderView = (
                         <div className='box-middle-panel mt-7'>
-                            <div className='position-relative'>
-                                <div className='dropdown'>
-                                    <a className='position-absolute top-0 end-0 onActioncomment text-dark' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>
-                                        <i className='bi-three-dots-vertical bi-layout-sidebar-inset-reverse-style'></i>
-                                    </a>
-                                    <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
-                                        <li><a className='dropdown-item onDeletecomment'>Delete</a></li>
-                                        <li><a className='dropdown-item onEditcomments'>Edit comment</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                            {dropdown}
                             {this.renderProfilePicture('fxl')}
                             <strong className='mt-2 ms-2 float-start'>@{name}</strong>
                             <br/>
                             <p className='mb-0 p-3 ms-2'>
                                 {postDesc}
                             </p>
-                            <form className='p-2 editcomments'>
-                                <div className='form-floating'>
-                                    <textarea className='form-control' placeholder='Edit comment' id='floatingTextarea2' style={{height: '100px',}}></textarea>
-                                    <label htmlFor='floatingTextarea2'>
-                                        Edit comment
-                                    </label>
-                                </div>
-                                <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white'>
-                                    Update
-                                </button>
-                                <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white onCanceledit'>
-                                    Cancel
-                                </button>
-                                <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white onDeletecomment'>
-                                    <i className='bi-trash-fill bi-trash-fill-style'></i>
-                                </button>
-                            </form>
+                            {editCommentValue}
                         </div>
                     );
                 }else{
                     renderView = (
                         <div className='box-middle-panel mt-7'>
-                            <div className='position-relative'>
-                                <div className='dropdown'>
-                                    <a className='position-absolute top-0 end-0 onActioncomment text-dark' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>
-                                        <i className='bi-three-dots-vertical bi-layout-sidebar-inset-reverse-style'></i>
-                                    </a>
-                                    <ul className='dropdown-menu dropdown-menu-dark' aria-labelledby='dropdownMenuButton1'>
-                                        <li><a className='dropdown-item onDeletecomment'>Delete</a></li>
-                                        <li><a className='dropdown-item onEditcomments'>Edit comment</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                            {dropdown}
                             {this.renderProfilePicture('fxl')}
                             <strong className='mt-2 ms-2 float-start'>@{name}</strong>
                             <br/>
                             <p className='mb-0 p-3 ms-2'>
                                 {postDesc}
                             </p>
-                            <form className='p-2 editcomments'>
-                                <div className='form-floating'>
-                                    <textarea className='form-control' placeholder='Edit comment' id='floatingTextarea2' style='height: 100px'></textarea>
-                                    <label htmlFor='floatingTextarea2'>
-                                        Edit comment
-                                    </label>
-                                </div>
-                                <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white'>
-                                    Update
-                                </button>
-                                <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white onCanceledit'>
-                                    Cancel
-                                </button>
-                                <button type='button' className='btn btn-creative btn-sm mt-2 mb-0 text-white onDeletecomment'>
-                                    <i className='bi-trash-fill bi-trash-fill-style'></i>
-                                </button>
-                            </form>
+                            {editCommentValue}
                         </div>
                     );
                 }
