ee44e6885dc00a608bab2d52dbd34e76a96cee2f
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-06 10:22:32 +0800

    Update create_post.tsx

--- components/create_post/create_post.tsx
+++ components/create_post/create_post.tsx
@@ -1506,7 +1506,7 @@ class CreatePost extends React.PureComponent<Props, State> {
                                         id: 'create_post.send_message',
                                         defaultMessage: 'Send a message',
                                     })}
-                                    className={sendButtonClass + 'pt-2'}
+                                    className={sendButtonClass + ' pt-2 me-2'}
                                     onClick={this.handleSubmit}
                                 >
                                     <LocalizedIcon
