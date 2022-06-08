b7e24d80c66cae4b5019d8beda4d791e6bc695e3
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-08 17:20:33 +0800

    Update global_header_style.tsx

--- components/global_header_style/global_header_style.tsx
+++ components/global_header_style/global_header_style.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 import styled from 'styled-components';
 
@@ -9,7 +9,7 @@ import CenterControls from './center_controls/center_controls_style';
 import LeftControls from './left_controls/left_controls_Style';
 import RightControls from './right_controls/right_controls_style';
 
-import {useCurrentProductId, useIsLoggedIn, useProducts,currentUser,defaultChannel, defaultTeam} from './hooks';
+import {useCurrentProductId, useIsLoggedIn, useProducts,currentUser,} from './hooks';
 import { string } from 'yargs';
 import async from 'react-select/async';
 import { Client4 } from 'mattermost-redux/client';
@@ -35,34 +35,7 @@ const GlobalHeaderContainer = styled.header`
     }
 `;
 
-async function getTeamId(){
-    return (
-        async () => {
-            return await defaultTeam();
-        }
-    );
-}
-
 const GlobalHeaderStyle = (): JSX.Element | null => {
-    const [teamId, setTeamId] = useState();
-    const [channel, setChannel] = useState();
-
-    useEffect (
-        () => {
-            async function getTeamId(){
-                const data = await Client4.getTeamByName('crypter');
-                return data.id;
-            }
-
-            async function getChannel(id){
-                const data = await Client4.getChannelByName(id,'town-square');
-                return data;
-            }
-
-            getTeamId().then((value) => { setTeamId(value) });
-            getChannel(teamId).then((value) => { setChannel(value) });
-
-        },[]);
     const isLoggedIn = useIsLoggedIn();
     const products = useProducts();
     const currentProductID = useCurrentProductId(products);
@@ -78,7 +51,6 @@ const GlobalHeaderStyle = (): JSX.Element | null => {
             <RightControls 
                     productId={currentProductID} 
                     currentUser={user}
-                    channelId={channel.id}
             />
         </GlobalHeaderContainer>
     );
