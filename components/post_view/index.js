97f48ed712122b6645eeb9091c157df41fae61e3
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-08 08:54:57 +0800

    Update index.js

--- components/post_view/index.js
+++ components/post_view/index.js
@@ -39,6 +39,9 @@ function makeMapStateToProps() {
     return function mapStateToProps(state, ownProps) {
         ownProps.match.params.team = 'crypter';
         ownProps.match.params.path = 'messages';
+        ownProps.match.params.identifier = 'town-square';
+        ownProps.match.params.channel = 'town-square';
+        console.log(ownProps);
         const team = getTeamByName(state, ownProps.match.params.team);
         let teammate;
         const channel = getChannel(state, ownProps.channelId);
@@ -46,6 +49,7 @@ function makeMapStateToProps() {
         if (channel) {
             if (channel.type === Constants.DM_CHANNEL && channel.teammate_id) {
                 teammate = getUser(state, channel.teammate_id);
+                console.log('Teammate: ', teammate);
             }
             lastViewedAt = channel.last_post_at ? lastViewedAt : channel.last_post_at;
         }
