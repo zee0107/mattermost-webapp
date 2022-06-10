5f3a9116a4fc19cd3065e4b244dac673ac21af08
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-10 11:53:17 +0800

    Update index.ts

--- components/global_header_style/right_controls/message_list/messages_direct/index.ts
+++ components/global_header_style/right_controls/message_list/messages_direct/index.ts
@@ -20,7 +20,6 @@ type OwnProps = {
 
 function makeMapStateToProps() {
     const getUnreadCount = makeGetChannelUnreadCount();
-    let valueChannel: ServerChannel;
 
     return function mapStateToProps(state: GlobalState, ownProps: OwnProps) {
         if(!state.entities.teams.teams && !state.entities.teams.teams.length){
@@ -29,8 +28,8 @@ function makeMapStateToProps() {
         }  
         const currentUser = getCurrentUser(state);
         const channelValue = Client4.getChannel(ownProps.channelId);
-        const channel = Promise.resolve(channelValue).then(value => { return value });
-        const userValue = Client4.getUser(valueChannel.teammate_id);
+        const channel = Promise.resolve(channelValue).then(value => value);
+        const userValue = Client4.getUser(channel.teammate_id);
         const teammate = Promise.resolve(userValue).then(value => value);
         //const unreadCount = Client4.getPostsUnread(ownProps.channelId, currentUser.id);
         const currentTeam = getTeamByName(state, 'crypter');
