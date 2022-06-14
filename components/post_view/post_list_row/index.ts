ac1861b98f8e8a77709060d048c4e22847a69787
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-14 10:14:08 +0800

    Updated QA

--- components/post_view/post_list_row/index.ts
+++ components/post_view/post_list_row/index.ts
@@ -22,6 +22,7 @@ type Props = {
 }
 
 function mapStateToProps(state: GlobalState, ownProps: Props) {
+    console.log('Row: ',state);
     const shortcutReactToLastPostEmittedFrom = getShortcutReactToLastPostEmittedFrom(state);
 
     return {
