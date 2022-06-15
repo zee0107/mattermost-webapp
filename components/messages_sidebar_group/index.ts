157e2d45c72d8d004d26d6c75ef2e9aa07f1c61a
John Renzeeh Reformo <jrreformo@polywickstudio.ph>
2022-06-15 15:02:07 +0800

    Update index.ts

--- components/messages_sidebar_group/index.ts
+++ components/messages_sidebar_group/index.ts
@@ -64,7 +64,7 @@ function makeMapStateToProps() {
         
         return {
             channel,
-            lastPostAt
+            lastPostAt,
             posts,
             membersCount,
             currentUser,
